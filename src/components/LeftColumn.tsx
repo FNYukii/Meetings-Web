e093af927d290633b08b634e3230a8725d503868
Yu357 <forestgreen14689@gmail.com>
2022-09-30 12:21:58 +0900

    feat: NavLinkを配置

--- src/components/LeftColumn.tsx
+++ src/components/LeftColumn.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { NavLink } from 'react-router-dom'
+
 export default function LeftColumn() {
 
     const Root = styled.div`
@@ -10,6 +12,14 @@ export default function LeftColumn() {
     return (
         <Root>
             <p>Side Bar</p>
+
+            <ul>
+                <li>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/search'>Search</NavLink>
+                    <NavLink to='/notifications'>Notifications</NavLink>
+                </li>
+            </ul>
         </Root>
     )
 }
