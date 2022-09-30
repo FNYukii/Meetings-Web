3001801fc01d99e105882c50da2475f82562b187
Yu357 <forestgreen14689@gmail.com>
2022-09-30 11:12:16 +0900

    feat: React Router導入

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+export default function App() {
+    return (
+        <React.StrictMode>
+            <BrowserRouter>
+                <Routes>
+                </Routes>
+            </BrowserRouter>
+        </React.StrictMode>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+    document.getElementById('root') as HTMLElement
+)
+
 root.render(
-  <React.StrictMode>
-    <h1>Hello</h1>
-  </React.StrictMode>
-);
+    <App/>
+)
