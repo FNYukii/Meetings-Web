241e61efa493cd3bb55eaa6d1b97cd192b8778d6
Yu357 <forestgreen14689@gmail.com>
2022-10-04 13:50:38 +0900

    feat: UserIconNavLink製作中

--- src/components/parts/ThreadRow.tsx
+++ src/components/parts/ThreadRow.tsx
@@ -1,7 +1,12 @@
 import Thread from "../../types/Thread"
+import UserIconNavLink from "./UserIconNavLink"
 
 export default function ThreadRow(props: {thread: Thread}) {
     return (
-        <div>{props.thread.title}</div>
+        <div>
+
+            <UserIconNavLink userId={props.thread.userId}/>
+            {props.thread.title}
+        </div>
     )
 }
