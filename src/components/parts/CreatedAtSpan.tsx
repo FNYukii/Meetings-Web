ca29689f962d29e3b0b2b385d359e781cb85d46e
Yu357 <forestgreen14689@gmail.com>
2022-10-04 15:28:45 +0900

    feat: 日時テキストを仮配置

---
+++ src/components/parts/CreatedAtSpan.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default function CreatedAtSpan() {
+
+    const StyledSpan = styled.span`
+        color: gray;
+    `
+
+    return (
+        <StyledSpan>10h</StyledSpan>
+    )
+}
