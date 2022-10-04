f93fd6732477de94519277abdebd12c3cdaaa887
Yu357 <forestgreen14689@gmail.com>
2022-10-04 17:58:53 +0900

    feat: Styled ComponentsからTailwind CSSへ移行完了

--- src/components/LeftColumn.tsx
+++ src/components/LeftColumn.tsx
@@ -1,47 +1,22 @@
-import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 import { FaHome, FaSearch } from 'react-icons/fa'
 
 export default function LeftColumn() {
-
-    const NavItemNavLink = styled(NavLink)`
-        display: flex;
-        justify-content: left;
-
-        color: inherit;
-        text-decoration: none;
-        padding: 8px;
-        border-radius: 32px;
-        margin-right: 8px;
-
-        &:hover {
-            background-color: #AAA3;
-        }
-
-        & *:first-child {
-            font-size: x-large;
-        }
-
-        & *:last-child {
-            margin-left: 4px;
-            font-size: large;
-        }
-    `
-
+    
     return (
         <div className='border w-1/4'>
             <NavLink to='/' className="text-inherit no-underline p-2 text-2xl">Meetings</NavLink>
 
             <nav>
-                <NavItemNavLink to='/'>
-                    <FaHome />
-                    <span>ホーム</span>
-                </NavItemNavLink>
+                <NavLink to='/' className="flex p-2 mr-2 hover:opacity-60">
+                    <FaHome className='text-xl'/>
+                    <span className='ml-1 text-lg'>ホーム</span>
+                </NavLink>
 
-                <NavItemNavLink to='/search'>
-                    <FaSearch />
-                    <span>検索</span>
-                </NavItemNavLink>
+                <NavLink to='/search' className="flex p-2 mr-2 hover:opacity-60">
+                    <FaSearch className='text-xl'/>
+                    <span className='ml-1 text-lg'>検索</span>
+                </NavLink>
 
             </nav>
         </div>
