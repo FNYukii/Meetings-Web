73a6eb286b674acceb63cf6932e5619e0c14acfa
Yu357 <forestgreen14689@gmail.com>
2022-10-04 13:53:19 +0900

    feat: Userエンティティを作成

---
+++ src/types/User.ts
@@ -0,0 +1,11 @@
+type User = {
+    id: string
+    userTag: string
+
+    displayName: string
+    introduction: string
+    iconUrl: string
+    likedCommentIds: string[]
+}
+
+export default User
