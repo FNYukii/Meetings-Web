a43c3c01112da5297e8382f786ec5eb43ca948a8
Yu357 <forestgreen14689@gmail.com>
2022-10-05 12:57:33 +0900

    feat: コードを簡略化

--- src/utilities/EditDate.ts
+++ src/utilities/EditDate.ts
@@ -1,30 +1,29 @@
 export default class EditDate {
 
     static howManyAgo(from: Date): string {
-        let posted = new Date(from)
-        let diff = new Date().getTime() - posted.getTime()
-        let progress = new Date(diff)
+        // 差分を生成
+        let diff = new Date(new Date().getTime() - from.getTime())
 
-        if (progress.getUTCFullYear() - 1970) {
-            return progress.getUTCFullYear() - 1970 + '年前'
+        if (diff.getUTCFullYear() - 1970) {
+            return diff.getUTCFullYear() - 1970 + '年前'
         }
         
-        if (progress.getUTCMonth()) {
-            return progress.getUTCMonth() + 'ヶ月前'
+        if (diff.getUTCMonth()) {
+            return diff.getUTCMonth() + 'ヶ月前'
         }
         
-        if (progress.getUTCDate() - 1) {
-            return progress.getUTCDate() - 1 + '日前'
+        if (diff.getUTCDate() - 1) {
+            return diff.getUTCDate() - 1 + '日前'
         }
         
-        if (progress.getUTCHours()) {
-            return progress.getUTCHours() + '時間前'
+        if (diff.getUTCHours()) {
+            return diff.getUTCHours() + '時間前'
         }
         
-        if (progress.getUTCMinutes()) {
-            return progress.getUTCMinutes() + '分前'
+        if (diff.getUTCMinutes()) {
+            return diff.getUTCMinutes() + '分前'
         }
         
-        return progress.getUTCSeconds() + '秒前'
+        return diff.getUTCSeconds() + '秒前'
     }
 }
