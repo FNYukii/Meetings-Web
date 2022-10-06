cc01bcf2268944ed2c5eb91953f59642b8c5f3b9
Yu357 <forestgreen14689@gmail.com>
2022-10-06 20:08:14 +0900

    feat: サイドバーで使うアイコンを変更

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom'
-import { FaHome, FaSearch } from 'react-icons/fa'
+import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai'
 
 export default function LeftColumn() {
     
@@ -9,12 +9,12 @@ export default function LeftColumn() {
 
             <nav className='mt-2'>
                 <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
-                    <FaHome className='text-2xl'/>
+                    <AiOutlineHome className='text-3xl'/>
                     <span className='ml-3 text-xl'>ホーム</span>
                 </NavLink>
 
                 <NavLink to='/search' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
-                    <FaSearch className='text-2xl'/>
+                    <AiOutlineSearch className='text-3xl'/>
                     <span className='ml-3 text-xl'>検索</span>
                 </NavLink>
 
