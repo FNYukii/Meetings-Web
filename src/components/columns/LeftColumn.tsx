6c72522b28325f5436f7f70c19189161c622da5d
Yu357 <forestgreen14689@gmail.com>
2022-10-06 20:39:12 +0900

    feat: 検索画面とNot Found画面を設定

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -5,7 +5,7 @@ export default function LeftColumn() {
     
     return (
         <div className='w-1/4'>
-            <NavLink to='/' className="text-inherit no-underline p-2 text-4xl font-sans">Meetings</NavLink>
+            <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans">Meetings</NavLink>
 
             <nav className='mt-2'>
                 <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
