78d78141cad07d669de6be937276b21bf81dfd78
Yu357 <forestgreen14689@gmail.com>
2022-10-06 11:30:47 +0900

    chore: firebaseConfigを一時的に公開

---
+++ src/utilities/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBqFSOUcZ1DvJo4jQwOE1EtSS5DYjFT-h8",
+    authDomain: "meetings-7dad0.firebaseapp.com",
+    projectId: "meetings-7dad0",
+    storageBucket: "meetings-7dad0.appspot.com",
+    messagingSenderId: "394721067159",
+    appId: "1:394721067159:web:7e6cce9e83d095276e69d2",
+    measurementId: "G-1XJNGLCRDE"
+}
