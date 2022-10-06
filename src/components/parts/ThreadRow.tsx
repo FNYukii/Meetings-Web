5f2c41f9d81c9f4d1f6aa691414816874c97732d
Yu357 <forestgreen14689@gmail.com>
2022-10-06 09:38:32 +0900

    feat: スレッドタグを表示

--- src/components/parts/ThreadRow.tsx
+++ src/components/parts/ThreadRow.tsx
@@ -4,21 +4,26 @@ import ThreadMenu from "./ThreadMenu"
 import UserIconNavLink from "./UserIconNavLink"
 import UserUserTagSpan from "./UserUserTagSpan"
 
-export default function ThreadRow(props: {thread: Thread}) {
-
+export default function ThreadRow(props: { thread: Thread }) {
+    
     return (
         <div className="flex p-2">
-            <UserIconNavLink userId={props.thread.userId}/>
+            <UserIconNavLink userId={props.thread.userId} />
 
             <div className="pl-2 w-full">
                 <div className="flex justify-between">
                     <span className="font-bold">{props.thread.title}</span>
-                    <ThreadMenu thread={props.thread}/>
+                    <ThreadMenu thread={props.thread} />
                 </div>
 
                 <div>
-                    <UserUserTagSpan userId={props.thread.userId}/>
+                    <UserUserTagSpan userId={props.thread.userId} />
                     <span className="text-gray-500 ml-2">{EditDate.howManyAgo(props.thread.createdAt)}</span>
+
+                    {props.thread.tags?.map((tag) => (
+                        <span key={tag} className="ml-2">{tag}</span>
+                    ))}
+
                 </div>
 
             </div>
