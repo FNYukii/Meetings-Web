9dc71a32c44395629b8b74ffa45a9b7c8adaa702
Yu357 <forestgreen14689@gmail.com>
2022-10-06 21:06:21 +0900

    feat: large-container設定

--- src/components/columns/RightColumn.tsx
+++ src/components/columns/RightColumn.tsx
@@ -1,8 +1,8 @@
 export default function RightColumn() {
 
     return (
-        <div className='w-1/4'>
-            <div className="m-2 bg-zinc-100 dark:bg-zinc-900 w-full h-96 rounded-lg p-2">
+        <div className='w-1/4 p-2'>
+            <div className="bg-zinc-100 dark:bg-zinc-900 w-full h-96 rounded-lg p-2">
                 <p className="font-bold text-lg">トレンド</p>
             </div>
         </div>
