48913dcf3c3789ddd5022ac89e4535df862442b2
Yu357 <forestgreen14689@gmail.com>
2022-10-06 19:33:58 +0900

    feat: Tailwind CSSのdarkバリアンとを試した

--- src/components/columns/RightColumn.tsx
+++ src/components/columns/RightColumn.tsx
@@ -2,7 +2,7 @@ export default function RightColumn() {
 
     return (
         <div className='w-1/4'>
-            <div className="m-2 bg-gray-300 bg-opacity-20 w-full h-96 rounded-lg p-2">
+            <div className="m-2 bg-gray-100 dark:bg-zinc-800 w-full h-96 rounded-lg p-2">
                 <p className="font-bold text-lg">トレンド</p>
             </div>
         </div>
