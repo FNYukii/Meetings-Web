2da6a27fe4b7cd6808d25d92a7f6932f9fb22841
Yu357 <forestgreen14689@gmail.com>
2022-10-06 09:20:50 +0900

    feat: ThreadRowにユーザータグを配置

---
+++ src/components/parts/UserUserTagSpan.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import User from "../../types/User"
+import FireUser from "../../utilities/FireUser"
+
+export default function UserUserTagSpan(props: {userId: string}) {
+
+    const [user, setUser] = useState<User | null>()
+
+    async function read() {
+        const user: User | null = await FireUser.readUserFromCache(props.userId)
+        setUser(user)
+    }
+
+    useEffect(() => {
+        read()
+        // eslint-disable-next-line
+    }, [])
+
+    return (
+        <span className="text-gray-500">@{user?.userTag}</span>
+    )
+}
