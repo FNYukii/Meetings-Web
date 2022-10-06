c79c1d43a783a6f2da33e9b3a7529b844aceca07
Yu357 <forestgreen14689@gmail.com>
2022-10-07 01:39:21 +0900

    feat: CommentRowのリンク先を設定

--- src/components/parts/CommentRow.tsx
+++ src/components/parts/CommentRow.tsx
@@ -3,10 +3,14 @@ import UserDisplayNameSpan from "./UserDisplayNameSpan"
 import UserIconNavLink from "./UserIconNavLink"
 import UserUserTagSpan from "./UserUserTagSpan"
 import EditDate from "../../utilities/EditDate"
+import { NavLink } from "react-router-dom"
 
 export default function CommentRow(props: { comment: Comment }) {
     return (
-        <div className="flex p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900">
+        <div className="flex p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900 relative">
+
+            <NavLink to={`/comments/${props.comment.id}`} className="absolute top-0 left-0 w-full h-full z-0" />
+
 
             <UserIconNavLink userId={props.comment.userId} />
 
