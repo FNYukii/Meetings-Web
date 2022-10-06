d3dc806e017df1ed490f1747d86cfac0776bdcb6
Yu357 <forestgreen14689@gmail.com>
2022-10-06 20:13:41 +0900

    feat: サイドバーの各項目内で中央揃え

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -8,12 +8,12 @@ export default function LeftColumn() {
             <NavLink to='/' className="text-inherit no-underline p-2 text-4xl font-sans">Meetings</NavLink>
 
             <nav className='mt-2'>
-                <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
+                <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
                     <AiOutlineHome className='text-3xl'/>
                     <span className='ml-3 text-xl'>ホーム</span>
                 </NavLink>
 
-                <NavLink to='/search' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
+                <NavLink to='/search' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
                     <AiOutlineSearch className='text-3xl'/>
                     <span className='ml-3 text-xl'>検索</span>
                 </NavLink>
