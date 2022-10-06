994ac04052562830e710e02ef5184d3e65737285
Yu357 <forestgreen14689@gmail.com>
2022-10-07 01:36:17 +0900

    fix: aタグ内にaタグがある問題を修正

--- src/components/parts/CommentRow.tsx
+++ src/components/parts/CommentRow.tsx
@@ -2,20 +2,20 @@ import Comment from "../../types/Comment"
 import UserDisplayNameSpan from "./UserDisplayNameSpan"
 import UserIconNavLink from "./UserIconNavLink"
 import UserUserTagSpan from "./UserUserTagSpan"
-import { NavLink } from "react-router-dom"
 import EditDate from "../../utilities/EditDate"
 
-export default function CommentRow(props: {comment: Comment}) {
+export default function CommentRow(props: { comment: Comment }) {
     return (
-        <NavLink to="/" className="flex p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900">
-            <UserIconNavLink userId={props.comment.userId}/>
+        <div className="flex p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900">
+
+            <UserIconNavLink userId={props.comment.userId} />
 
             <div className="pl-3 w-full">
                 <div>
-                    <UserDisplayNameSpan userId={props.comment.userId}/>
+                    <UserDisplayNameSpan userId={props.comment.userId} />
 
                     <span className="ml-3">
-                        <UserUserTagSpan userId={props.comment.userId}/>
+                        <UserUserTagSpan userId={props.comment.userId} />
                     </span>
 
                     <span className="text-gray-500 ml-3">{EditDate.howManyAgo(props.comment.createdAt)}</span>
@@ -26,6 +26,6 @@ export default function CommentRow(props: {comment: Comment}) {
                 </div>
             </div>
 
-        </NavLink>
+        </div>
     )
 }
