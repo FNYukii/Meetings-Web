0a5ab7d8b5404909a14fa6e12399c443c9b5eeec
Yu357 <forestgreen14689@gmail.com>
2022-10-06 21:40:36 +0900

    clean

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -7,7 +7,7 @@ export default function LeftColumn() {
         <div className='xl:w-1/4 md:w-1/12 w-2/12'>
             <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans">Meetings</NavLink>
 
-            <nav className='mt-2'>
+            <div className='mt-2'>
                 <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
                     <AiOutlineHome className='text-3xl'/>
                     <span className='ml-3 text-xl xl:block hidden'>ホーム</span>
@@ -18,7 +18,7 @@ export default function LeftColumn() {
                     <span className='ml-3 text-xl xl:block hidden'>検索</span>
                 </NavLink>
 
-            </nav>
+            </div>
         </div>
     )
 }
