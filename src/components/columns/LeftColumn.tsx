6935ad7aceac7941fb92a191eb15ae0642d8b07c
Yu357 <forestgreen14689@gmail.com>
2022-10-06 22:04:12 +0900

    feat: 左右のカラムを固定した

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -4,8 +4,8 @@ import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai'
 export default function LeftColumn() {
 
     return (
-        <div className='xl:w-1/4 md:w-1/12 w-fit xl:block xl:justify-start flex justify-end p-2'>
-            <div>
+        <div className='xl:w-1/4 md:w-1/12 w-fit xl:block xl:justify-start flex justify-end'>
+            <div className='sticky top-0 p-2'>
                 <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
 
                 <div>
