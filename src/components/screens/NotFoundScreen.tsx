4e7803c0c753c14bfc2f4f89f951b9eca2b83040
Yu357 <forestgreen14689@gmail.com>
2022-10-06 23:21:23 +0900

    feat: テキスト調整

--- src/components/screens/NotFoundScreen.tsx
+++ src/components/screens/NotFoundScreen.tsx
@@ -5,11 +5,11 @@ export default function NotFoundScreen() {
     return (
         <div>
             <div className='p-2 border-b border-zinc-200 dark:border-zinc-800'>
-                <span className='font-bold text-lg'>不明</span>
+                <span className='font-bold text-lg'>Not Found</span>
             </div>
 
             <div className="p-2">
-                <p>ページが見つかりませんでした。</p>
+                <p className="text-gray-500 text-center">ページが見つかりませんでした。</p>
             </div>
         </div>
     )
