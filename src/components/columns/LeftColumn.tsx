b611a3b9b94d89e90cc218a4f45bad5d101b9c43
Yu357 <forestgreen14689@gmail.com>
2022-10-06 19:55:10 +0900

    feat: サイドバーをスタイリング

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -5,17 +5,17 @@ export default function LeftColumn() {
     
     return (
         <div className='w-1/4'>
-            <NavLink to='/' className="text-inherit no-underline p-2 text-2xl">Meetings</NavLink>
+            <NavLink to='/' className="text-inherit no-underline p-2 text-4xl font-sans">Meetings</NavLink>
 
             <nav>
-                <NavLink to='/' className="flex p-2 mr-2 hover:opacity-60">
-                    <FaHome className='text-xl'/>
-                    <span className='ml-1 text-lg'>ホーム</span>
+                <NavLink to='/' className="flex p-2 mr-2 mt-2 hover:opacity-60">
+                    <FaHome className='text-2xl'/>
+                    <span className='ml-2 text-xl'>ホーム</span>
                 </NavLink>
 
                 <NavLink to='/search' className="flex p-2 mr-2 hover:opacity-60">
-                    <FaSearch className='text-xl'/>
-                    <span className='ml-1 text-lg'>検索</span>
+                    <FaSearch className='text-2xl'/>
+                    <span className='ml-2 text-xl'>検索</span>
                 </NavLink>
 
             </nav>
