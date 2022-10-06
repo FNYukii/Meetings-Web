edf0c305f2624475ea9b8806062fda03b799f91c
Yu357 <forestgreen14689@gmail.com>
2022-10-06 20:04:50 +0900

    feat: マージン調整

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -7,15 +7,15 @@ export default function LeftColumn() {
         <div className='w-1/4'>
             <NavLink to='/' className="text-inherit no-underline p-2 text-4xl font-sans">Meetings</NavLink>
 
-            <nav>
-                <NavLink to='/' className="flex p-2 mr-2 mt-2 hover:opacity-60">
+            <nav className='mt-2'>
+                <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
                     <FaHome className='text-2xl'/>
-                    <span className='ml-2 text-xl'>ホーム</span>
+                    <span className='ml-3 text-xl'>ホーム</span>
                 </NavLink>
 
-                <NavLink to='/search' className="flex p-2 mr-2 hover:opacity-60">
+                <NavLink to='/search' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
                     <FaSearch className='text-2xl'/>
-                    <span className='ml-2 text-xl'>検索</span>
+                    <span className='ml-3 text-xl'>検索</span>
                 </NavLink>
 
             </nav>
