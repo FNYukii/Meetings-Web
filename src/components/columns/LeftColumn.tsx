f9f5010497c95141725dd71e09f7545995dd2b29
Yu357 <forestgreen14689@gmail.com>
2022-10-06 21:53:24 +0900

    feat: サイドバーアイテムのmargin調整

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -4,17 +4,17 @@ import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai'
 export default function LeftColumn() {
 
     return (
-        <div className='xl:w-1/4 md:w-1/12 w-2/12 xl:block xl:justify-start flex justify-end'>
+        <div className='xl:w-1/4 md:w-1/12 w-fit xl:block xl:justify-start flex justify-end p-2'>
             <div>
                 <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
 
-                <div className='mt-2'>
-                    <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
+                <div>
+                    <NavLink to='/' className="flex p-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
                         <AiOutlineHome className='text-3xl' />
                         <span className='ml-3 text-xl xl:block hidden'>ホーム</span>
                     </NavLink>
 
-                    <NavLink to='/search' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
+                    <NavLink to='/search' className="flex p-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
                         <AiOutlineSearch className='text-3xl' />
                         <span className='ml-3 text-xl xl:block hidden'>検索</span>
                     </NavLink>
