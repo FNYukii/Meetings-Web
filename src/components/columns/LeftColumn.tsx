9e82e1b0f423dc7aef6cc78e3c51adf031a3c514
Yu357 <forestgreen14689@gmail.com>
2022-10-06 21:39:30 +0900

    feat: メディアクエリは三段階設定した

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -4,18 +4,18 @@ import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai'
 export default function LeftColumn() {
     
     return (
-        <div className='md:w-1/4 w-1/3'>
+        <div className='xl:w-1/4 md:w-1/12 w-2/12'>
             <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans">Meetings</NavLink>
 
             <nav className='mt-2'>
                 <NavLink to='/' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
                     <AiOutlineHome className='text-3xl'/>
-                    <span className='ml-3 text-xl'>ホーム</span>
+                    <span className='ml-3 text-xl xl:block hidden'>ホーム</span>
                 </NavLink>
 
                 <NavLink to='/search' className="flex p-2 mr-2 mt-1 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
                     <AiOutlineSearch className='text-3xl'/>
-                    <span className='ml-3 text-xl'>検索</span>
+                    <span className='ml-3 text-xl xl:block hidden'>検索</span>
                 </NavLink>
 
             </nav>
