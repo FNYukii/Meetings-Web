a1fa7221b5162876efd4982284dc0c9aad62a16e
Yu357 <forestgreen14689@gmail.com>
2022-10-07 21:21:49 +0900

    feat: CommentRowに画像を表示

--- src/components/columns/RightColumn.tsx
+++ src/components/columns/RightColumn.tsx
@@ -3,7 +3,7 @@ export default function RightColumn() {
     return (
         <div className='xl:w-1/4 md:w-4/12 md:block hidden'>
             <div className="sticky top-0 px-4 pt-2">
-                <div className="bg-zinc-100 dark:bg-zinc-900 w-full h-96 rounded-lg p-3">
+                <div className="bg-zinc-100 dark:bg-zinc-900 w-full h-96 rounded-xl p-3">
                     <p className="font-bold text-lg">トレンド</p>
                 </div>
             </div>
--- src/components/parts/CommentRow.tsx
+++ src/components/parts/CommentRow.tsx
@@ -34,6 +34,14 @@ export default function CommentRow(props: { comment: Comment }) {
                 <div>
                     <p>{props.comment.text}</p>
                 </div>
+
+                <div className="flex gap-2">
+                    {props.comment.imageUrls.map((url) => (
+                        <div key={url} className="z-10 mt-2 hover:opacity-80">
+                            <img src={url} alt="Attached to comment" className="rounded-xl"/>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
