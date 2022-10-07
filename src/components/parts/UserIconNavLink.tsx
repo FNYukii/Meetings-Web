afa5a4d70314eeb7c15f506a41ade5d3915dc6cd
Yu357 <forestgreen14689@gmail.com>
2022-10-07 12:13:48 +0900

    clean: 不要なコードを削除

--- src/components/parts/UserIconNavLink.tsx
+++ src/components/parts/UserIconNavLink.tsx
@@ -18,11 +18,11 @@ export default function UserIconNavLink(props: { userId: string }) {
     }, []);
 
     return (
-        <div className="hover:opacity-60 z-10 relative w-12 h-11">
+        <div className="hover:opacity-60 z-10 relative w-12 aspect-square">
 
-            <div className="bg-zinc-200 dark:bg-zinc-800 rounded-full absolute top-0 left-0 w-full h-full"></div>
+            <div className="bg-zinc-200 dark:bg-zinc-800 rounded-full absolute top-0 left-0 w-full aspect-square"></div>
             
-            <img className="w-12 rounded-full absolute top-0 left-0 w-full" src={user?.iconUrl} alt="" />
+            <img className="rounded-full absolute top-0 left-0 w-full" src={user?.iconUrl} alt="" />
 
             <NavLink to={`/users/${props.userId}`} className="absolute top-0 left-0 w-full h-full" />
         </div>
