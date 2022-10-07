f3ab1f6d35fb60cbcb4950c5716d8f26d4ad8c20
Yu357 <forestgreen14689@gmail.com>
2022-10-07 11:43:19 +0900

    feat: TitleBarにbackdrop-blurを設定

--- src/components/parts/TitleBar.tsx
+++ src/components/parts/TitleBar.tsx
@@ -1,6 +1,6 @@
 export default function TitleBar(props: { text: string }) {
     return (
-        <div className='p-2 border-b border-zinc-200 dark:border-zinc-800 sticky top-0 bg-white dark:bg-black z-20'>
+        <div className='p-2 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur'>
             <span className='font-bold text-lg'>{props.text}</span>
         </div>
     )
