32d6199c6c24d90bc61efc0c2804c0dcc2c48b18
Yu357 <forestgreen14689@gmail.com>
2022-10-07 12:29:10 +0900

    feat: 戻るボタンを配置

--- src/components/parts/TitleBar.tsx
+++ src/components/parts/TitleBar.tsx
@@ -1,7 +1,18 @@
+import { AiOutlineArrowLeft } from "react-icons/ai"
+import { useNavigate } from "react-router-dom"
+
 export default function TitleBar(props: { text: string }) {
+
+    const navigate = useNavigate()
+
     return (
-        <div className='p-2 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur mb-1'>
-            <span className='font-bold text-lg'>{props.text}</span>
+        <div className='p-2 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur mb-1 flex items-center'>
+
+            <button onClick={() => navigate(-1)} className="p-2 text-xl hover:bg-zinc-100 rounded-full">
+                <AiOutlineArrowLeft />
+            </button>
+
+            <span className='font-bold text-lg ml-2'>{props.text}</span>
         </div>
     )
 }
