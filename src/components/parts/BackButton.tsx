27079dbee00c3336da9896bdf4f2b2be7e56a7fd
Yu357 <forestgreen14689@gmail.com>
2022-10-07 20:39:11 +0900

    clean: TitleBarコンポーネントを削除

---
+++ src/components/parts/BackButton.tsx
@@ -0,0 +1,13 @@
+import { FiArrowLeft } from "react-icons/fi"
+import { useNavigate } from "react-router-dom"
+
+export default function BackButton() {
+
+    const navigate = useNavigate()
+
+    return (
+        <button onClick={() => navigate(-1)} className="p-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full mr-3">
+            <FiArrowLeft className="text-2xl" />
+        </button>
+    )
+}
--- src/components/screens/SearchScreen.tsx
+++ src/components/screens/SearchScreen.tsx
@@ -1,12 +1,12 @@
-import TitleBar from "../parts/TitleBar"
-
 export default function SearchScreen() {
 
     document.title = "検索 - Meetings"
 
     return (
         <div>
-            <TitleBar text="検索" isShowBackButton={false}/>
+            <div className='h-14 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur px-3 flex items-center'>
+                <span className='font-bold text-lg'>検索</span>
+            </div>
         </div>
     )
 }
