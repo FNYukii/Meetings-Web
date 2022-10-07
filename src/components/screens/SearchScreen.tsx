e87cec87be510db2848f489adc7c62bfc5d096df
Yu357 <forestgreen14689@gmail.com>
2022-10-07 21:00:38 +0900

    feat: 検索バー作ってみた

--- src/components/screens/SearchScreen.tsx
+++ src/components/screens/SearchScreen.tsx
@@ -5,7 +5,7 @@ export default function SearchScreen() {
     return (
         <div>
             <div className='h-14 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur px-3 flex items-center'>
-                <span className='font-bold text-lg'>検索</span>
+                <input type="text" className="bg-zinc-100 dark:bg-zinc-900 outline-blue-500 py-2 px-4 rounded-full w-full" placeholder="キーワード"/>
             </div>
         </div>
     )
