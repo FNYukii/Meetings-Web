172e31f92092b9b9cec5f730a3ad4b715ef59030
Yu357 <forestgreen14689@gmail.com>
2022-10-07 15:38:10 +0900

    feat: スタイリング

--- src/components/parts/TitleBar.tsx
+++ src/components/parts/TitleBar.tsx
@@ -6,15 +6,15 @@ export default function TitleBar(props: { text: string, isShowBackButton: boolea
     const navigate = useNavigate()
 
     return (
-        <div className='p-2 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur mb-1 flex items-center border-b'>
-            
+        <div className='sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur mb-1 flex items-center h-14 px-3'>
+
             {props.isShowBackButton &&
-                <button onClick={() => navigate(-1)} className="p-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
+                <button onClick={() => navigate(-1)} className="p-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full mr-3">
                     <FiArrowLeft className="text-2xl"/>
                 </button>
             }
 
-            <span className='font-bold text-lg ml-2'>{props.text}</span>
+            <span className='font-bold text-lg'>{props.text}</span>
         </div>
     )
 }
