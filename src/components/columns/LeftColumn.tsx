32119f415654ea716685260afb79b8a718999889
Yu357 <forestgreen14689@gmail.com>
2022-10-07 22:14:14 +0900

    fix: 画面サイズが小さい時、LeftColumnにstickyが効かない現象を修正

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -4,30 +4,32 @@ import { AiOutlineHome, AiOutlineSearch, AiOutlineLogin, AiOutlineEdit } from 'r
 export default function LeftColumn() {
 
     return (
-        <div className='xl:w-1/4 md:w-1/12 w-fit xl:block xl:justify-start flex justify-end'>
-            <div className='sticky top-0 xl:px-4 px-2'>
-                <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
+        <div className='xl:w-1/4 md:w-1/12 w-fit xl:justify-start'>
+            <div className='sticky top-0'>
+                <div className='xl:block flex justify-end xl:px-4 px-2'>
+                    <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
 
-                <div>
-                    <NavLink to='/' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
-                        <AiOutlineHome className='text-3xl' />
-                        <span className='ml-5 text-xl xl:block hidden'>ホーム</span>
-                    </NavLink>
+                    <div>
+                        <NavLink to='/' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
+                            <AiOutlineHome className='text-3xl' />
+                            <span className='ml-5 text-xl xl:block hidden'>ホーム</span>
+                        </NavLink>
 
-                    <NavLink to='/search' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
-                        <AiOutlineSearch className='text-3xl' />
-                        <span className='ml-5 text-xl xl:block hidden'>検索</span>
-                    </NavLink>
+                        <NavLink to='/search' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
+                            <AiOutlineSearch className='text-3xl' />
+                            <span className='ml-5 text-xl xl:block hidden'>検索</span>
+                        </NavLink>
 
-                    <NavLink to='/sign-in' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
-                        <AiOutlineLogin className='text-3xl' />
-                        <span className='ml-5 text-xl xl:block hidden'>サインイン</span>
-                    </NavLink>
+                        <NavLink to='/sign-in' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
+                            <AiOutlineLogin className='text-3xl' />
+                            <span className='ml-5 text-xl xl:block hidden'>サインイン</span>
+                        </NavLink>
 
-                    <NavLink to='/sign-up' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
-                        <AiOutlineEdit className='text-3xl' />
-                        <span className='ml-5 text-xl xl:block hidden'>サインアップ</span>
-                    </NavLink>
+                        <NavLink to='/sign-up' className="flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center">
+                            <AiOutlineEdit className='text-3xl' />
+                            <span className='ml-5 text-xl xl:block hidden'>サインアップ</span>
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </div>
