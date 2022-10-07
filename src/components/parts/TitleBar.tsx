fe70e371cb8529af4ef8f7ae711f7659432e20d5
Yu357 <forestgreen14689@gmail.com>
2022-10-07 11:52:40 +0900

    feat: ThreadMenuのhover状態を改善

--- src/components/parts/TitleBar.tsx
+++ src/components/parts/TitleBar.tsx
@@ -1,6 +1,6 @@
 export default function TitleBar(props: { text: string }) {
     return (
-        <div className='p-2 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur'>
+        <div className='p-2 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur mb-1'>
             <span className='font-bold text-lg'>{props.text}</span>
         </div>
     )
