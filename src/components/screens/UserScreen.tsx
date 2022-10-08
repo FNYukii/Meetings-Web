0d58da1041569a0288eeb513b3f7ea51f9a85fea
Yu357 <forestgreen14689@gmail.com>
2022-10-08 17:50:03 +0900

    feat: UserScreenにUserIconを表示

--- src/components/screens/UserScreen.tsx
+++ src/components/screens/UserScreen.tsx
@@ -3,15 +3,19 @@ import { useParams } from "react-router-dom"
 import User from "../../types/User"
 import FireUser from "../../utilities/FireUser"
 import BackButton from "../parts/BackButton"
+import progress from "../../images/progress.svg"
+import UserIcon from "../parts/UserIcon"
 
 export default function UserScreen() {
 
     const { userId } = useParams()
     const [user, setUser] = useState<User | null>(null)
+    const [isLoaded, setIsLoaded] = useState(false)
 
     async function readUser() {
         const user = await FireUser.readUserFromCache(userId!)
         setUser(user)
+        setIsLoaded(true)
     }
 
     useEffect(() => {
@@ -29,10 +33,25 @@ export default function UserScreen() {
                 </div>
             </div>
 
-            <div className="p-3">
-                <p>{userId!}</p>
-                <p>{user?.displayName ?? ""}</p>
-            </div>
+            {!isLoaded &&
+                <div className='flex justify-center'>
+                    <img src={progress} alt='loading' />
+                </div>
+            }
+
+            {isLoaded && user === null &&
+                <div className="p-2">
+                    <p className="text-gray-500 text-center">読み取りに失敗しました。</p>
+                </div>
+            }
+
+            {isLoaded && user !== null &&
+                <div className="p-3">
+                    <UserIcon iconUrl={user!.iconUrl}/>
+                    <p>{userId!}</p>
+                    <p>{user!.displayName}</p>
+                </div>
+            }
         </div>
     )
 }
