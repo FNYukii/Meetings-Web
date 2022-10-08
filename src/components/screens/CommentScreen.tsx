9ae09abcb87a2b8313818e6bcec8eef564add9f8
Yu357 <forestgreen14689@gmail.com>
2022-10-08 15:46:57 +0900

    feat: スタイリング

--- src/components/screens/CommentScreen.tsx
+++ src/components/screens/CommentScreen.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Comment from "../../types/Comment"
+import EditDate from "../../utilities/EditDate"
 import FireComment from "../../utilities/FireComment"
+import BackButton from "../parts/BackButton"
+import CommentMenu from "../parts/CommentMenu"
+import ImagesGrid from "../parts/ImagesGrid"
+import UserDisplayNameSpan from "../parts/UserDisplayNameSpan"
+import UserIconNavLink from "../parts/UserIconNavLink"
+import UserUserTagSpan from "../parts/UserUserTagSpan"
 
 export default function CommentScreen() {
 
@@ -22,7 +29,36 @@ export default function CommentScreen() {
 
     return (
         <div>
-            <p>{comment?.text ?? ""}</p>
+            <div className='sticky top-0 z-20'>
+                <div className='relative h-14 px-3 flex items-center bg-white/70 dark:bg-black/70 backdrop-blur'>
+                    <div className='absolute top-0 left-0 w-full h-full cursor-pointer' onClick={() => window.scrollTo(0, 0)}></div>
+                    <BackButton />
+                    <span className='font-bold text-lg'>コメント</span>
+                </div>
+            </div>
+
+            {comment !== null &&
+                <div className="p-3">
+                    <div className="flex justify-between">
+                        <div className="flex gap-3">
+                            <UserIconNavLink userId={comment?.userId ?? ""} />
+
+                            <div className="flex flex-col">
+                                <UserDisplayNameSpan userId={comment!.userId} />
+                                <UserUserTagSpan userId={comment!.userId} />
+                            </div>
+
+                            <span className="text-gray-500">{EditDate.howManyAgo(comment!.createdAt)}</span>
+                        </div>
+
+                        <CommentMenu comment={comment!} />
+                    </div>
+
+                    <p className="mt-2">{comment!.text}</p>
+
+                    <ImagesGrid imageUrls={comment!.imageUrls} />
+                </div>
+            }
         </div>
     )
 }
