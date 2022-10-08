3833d1b3643103b73e8d6fcaa1c22f8cd1783dc8
Yu357 <forestgreen14689@gmail.com>
2022-10-09 08:49:43 +0900

    feat: ロゴの当たり判定を調整

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -8,7 +8,7 @@ export default function LeftColumn() {
         <div className='xl:w-1/4 md:w-1/12'>
             <div className='sticky top-0'>
                 <div className='xl:block xl:px-4 flex justify-end px-2 pt-1'>
-                    <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
+                    <NavLink to='/' className="text-inherit no-underline w-fit p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
 
                     <div>
                         <NavLink to='/' end className={({ isActive }) => `${isActive ? "font-bold" : ""}`}>
