8f156658f934a6ee3e86ad99a71491a7d81942ab
Yu357 <forestgreen14689@gmail.com>
2022-10-08 17:31:21 +0900

    feat: Title Barを配置

--- src/components/screens/UserScreen.tsx
+++ src/components/screens/UserScreen.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom"
+import BackButton from "../parts/BackButton"
 
 export default function UserScreen() {
 
@@ -6,6 +7,14 @@ export default function UserScreen() {
 
     return (
         <div>
+            <div className='sticky top-0 z-20'>
+                <div className='relative h-14 px-3 flex items-center bg-white/70 dark:bg-black/70 backdrop-blur'>
+                    <div className='absolute top-0 left-0 w-full h-full cursor-pointer' onClick={() => window.scrollTo(0, 0)}></div>
+                    <BackButton />
+                    <span className='font-bold text-lg'>プロフィール</span>
+                </div>
+            </div>
+
             <p>{userId!}</p>
         </div>
     )
