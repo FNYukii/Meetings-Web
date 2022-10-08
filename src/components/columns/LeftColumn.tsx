9062754c36d2644741caf55b8ab98721dc1faa5f
Yu357 <forestgreen14689@gmail.com>
2022-10-08 21:16:10 +0900

    feat: NavLinkのisActiveの値によって、アイコンを切り替えるようにした

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -1,28 +1,43 @@
 import { NavLink } from 'react-router-dom'
-import { AiOutlineHome, AiOutlineSearch, AiOutlineLogin } from 'react-icons/ai'
+import { AiOutlineHome, AiFillHome, AiOutlineSearch, AiOutlineLogin } from 'react-icons/ai'
 
 export default function LeftColumn() {
 
     return (
         <div className='xl:w-1/4 md:w-1/12'>
             <div className='sticky top-0'>
-                <div className='xl:block xl:px-4 flex justify-end px-2'>
-                    <NavLink to='/' className="text-inherit no-underline p-2 mt-1 text-3xl font-sans xl:block hidden">Meetings</NavLink>
+                <div className='xl:block xl:px-4 flex justify-end px-2 pt-1'>
+                    <NavLink to='/' className="text-inherit no-underline p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
 
                     <div>
-                        <NavLink to='/' end className={({ isActive }) => `flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center ${isActive ? "font-bold" : ""}`}>
-                            <AiOutlineHome className='text-3xl' />
-                            <span className='ml-5 text-xl xl:block hidden'>ホーム</span>
+                        <NavLink to='/' end className={({ isActive }) => `${isActive ? "font-bold" : ""}`}>
+                            {({ isActive }) => (
+                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 mt-2 rounded-full'>
+                                    <AiFillHome className={`text-3xl ${isActive ? "block": "hidden"}`}/>
+                                    <AiOutlineHome className={`text-3xl ${isActive ? "hidden": "block"}`}/>
+                                    <span className='ml-5 text-xl xl:block hidden'>ホーム</span>
+                                </div>
+                            )}
                         </NavLink>
 
-                        <NavLink to='/search' className={({ isActive }) => `flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center ${isActive ? "font-bold" : ""}`}>
-                            <AiOutlineSearch className='text-3xl' />
-                            <span className='ml-5 text-xl xl:block hidden'>検索</span>
+                        <NavLink to='/search' className={({ isActive }) => `${isActive ? "font-bold" : ""}`}>
+                            {({ isActive }) => (
+                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 mt-2 rounded-full'>
+                                    <AiFillHome className={`text-3xl ${isActive ? "block": "hidden"}`}/>
+                                    <AiOutlineSearch className={`text-3xl ${isActive ? "hidden": "block"}`}/>
+                                    <span className='ml-5 text-xl xl:block hidden'>検索</span>
+                                </div>
+                            )}
                         </NavLink>
 
-                        <NavLink to='/sign-in' className={({ isActive }) => `flex p-2 mt-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full items-center ${isActive ? "font-bold" : ""}`}>
-                            <AiOutlineLogin className='text-3xl' />
-                            <span className='ml-5 text-xl xl:block hidden'>サインイン</span>
+                        <NavLink to='/sign-in' end className={({ isActive }) => `${isActive ? "font-bold" : ""}`}>
+                            {({ isActive }) => (
+                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 mt-2 rounded-full'>
+                                    <AiFillHome className={`text-3xl ${isActive ? "block": "hidden"}`}/>
+                                    <AiOutlineLogin className={`text-3xl ${isActive ? "hidden": "block"}`}/>
+                                    <span className='ml-5 text-xl xl:block hidden'>サインイン</span>
+                                </div>
+                            )}
                         </NavLink>
                     </div>
                 </div>
