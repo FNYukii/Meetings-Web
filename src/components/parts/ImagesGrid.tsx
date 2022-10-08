97dfd2e042e157249a2880d471298e0347eb03b5
Yu357 <forestgreen14689@gmail.com>
2022-10-08 18:24:38 +0900

    feat: ImagesGridをレスポンシブ対応

--- src/components/parts/ImagesGrid.tsx
+++ src/components/parts/ImagesGrid.tsx
@@ -2,13 +2,13 @@ export default function ImagesGrid(props: { imageUrls: string[] }) {
     return (
         <div>
             {props.imageUrls.length === 1 &&
-                <div className="mt-2 h-80">
+                <div className="mt-2 lg:h-80 md:h-60 h-50">
                     <img src={props.imageUrls[0]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
                 </div>
             }
 
             {props.imageUrls.length === 2 &&
-                <div className="mt-2 flex h-80">
+                <div className="mt-2 flex lg:h-80 md:h-60 h-50">
                     <div className="w-1/2 h-full pr-1">
                         <img src={props.imageUrls[0]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
                     </div>
@@ -20,7 +20,7 @@ export default function ImagesGrid(props: { imageUrls: string[] }) {
             }
 
             {props.imageUrls.length === 3 &&
-                <div className="mt-2 flex h-80">
+                <div className="mt-2 flex lg:h-80 md:h-60 h-50">
 
                     <div className="w-1/2 h-full pr-1">
                         <img src={props.imageUrls[0]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
@@ -39,7 +39,7 @@ export default function ImagesGrid(props: { imageUrls: string[] }) {
             }
 
             {props.imageUrls.length === 4 &&
-                <div className="mt-2 flex h-80">
+                <div className="mt-2 flex lg:h-80 md:h-60 h-50">
 
                     <div className="w-1/2 h-full pr-1">
                         <div className="h-1/2 pb-1">
