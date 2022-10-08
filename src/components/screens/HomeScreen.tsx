9869406782991ac21337fa11aea360fc3f716dcb
Yu357 <forestgreen14689@gmail.com>
2022-10-08 12:10:05 +0900

    feat: HomeScreenのTitleBarを押すと、ページ最上部へスクロールするようにした

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -36,8 +36,13 @@ export default function HomeScreen() {
 
     return (
         <div>
-            <div className='h-14 sticky top-0 bg-white/70 dark:bg-black/70 z-20 backdrop-blur px-3 flex items-center'>
-                <span className='font-bold text-lg'>ホーム</span>
+            <div className='sticky top-0 z-20'>
+                <div className='relative h-14 px-3 flex items-center bg-white/70 dark:bg-black/70 backdrop-blur'>
+
+                    <div className='absolute top-0 left-0 w-full h-full cursor-pointer' onClick={() => window.scrollTo(0, 0)}></div>
+                    
+                    <span className='font-bold text-lg'>ホーム</span>
+                </div>
             </div>
 
             {!isLoaded &&
