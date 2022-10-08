1b9f0666b4cce5477ee6ebd059febd5e62c96034
Yu357 <forestgreen14689@gmail.com>
2022-10-08 14:39:51 +0900

    feat: 画像4枚の表示に対応

--- src/components/parts/ImagesGrid.tsx
+++ src/components/parts/ImagesGrid.tsx
@@ -39,8 +39,27 @@ export default function ImagesGrid(props: { imageUrls: string[] }) {
             }
 
             {props.imageUrls.length === 4 &&
-                <div>
+                <div className="mt-2 flex h-80">
 
+                    <div className="w-1/2 h-full pr-1">
+                        <div className="h-1/2 pb-1">
+                            <img src={props.imageUrls[0]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
+                        </div>
+
+                        <div className="h-1/2 pt-1">
+                            <img src={props.imageUrls[1]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
+                        </div>
+                    </div>
+
+                    <div className="w-1/2 h-full pl-1">
+                        <div className="h-1/2 pb-1">
+                            <img src={props.imageUrls[2]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
+                        </div>
+
+                        <div className="h-1/2 pt-1">
+                            <img src={props.imageUrls[3]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
+                        </div>
+                    </div>
                 </div>
             }
         </div>
