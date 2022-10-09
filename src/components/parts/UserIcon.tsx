6c256dbbfb6763d4013aced1a57613177c22f6fc
Yu357 <forestgreen14689@gmail.com>
2022-10-09 12:01:30 +0900

    feat: UserIconの大きさを調整

--- src/components/parts/UserIcon.tsx
+++ src/components/parts/UserIcon.tsx
@@ -1,6 +1,6 @@
 export default function UserIcon(props: {iconUrl: string}) {
     return (
-        <div className="relative w-20 h-20">
+        <div className="relative w-16 h-16">
 
             <div className="bg-zinc-200 dark:bg-zinc-800 rounded-full absolute top-0 left-0 w-full aspect-square"></div>
             
