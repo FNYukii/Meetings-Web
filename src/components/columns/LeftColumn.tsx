bac07d7bf6d86b871085cc7157336d53d73b8a87
Yu357 <forestgreen14689@gmail.com>
2022-10-09 09:00:56 +0900

    feat: Global Navの項目を押した際、画面の一番上までスクロールするようにした

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -8,34 +8,34 @@ export default function LeftColumn() {
         <div className='xl:w-1/4 md:w-1/12'>
             <div className='sticky top-0'>
                 <div className='xl:block xl:px-4 flex justify-end px-2 pt-1'>
-                    <NavLink to='/' className="text-inherit no-underline w-fit p-2 text-3xl font-sans xl:block hidden">Meetings</NavLink>
+                    <NavLink to='/' className="text-inherit no-underline w-fit p-2 text-3xl font-sans xl:block hidden" onClick={() => window.scrollTo(0, 0)}>Meetings</NavLink>
 
                     <div>
-                        <NavLink to='/' end className={({ isActive }) => `${isActive ? "font-bold" : ""}`}>
+                        <NavLink to='/' end className="block mt-2 rounded-full" onClick={() => window.scrollTo(0, 0)}>
                             {({ isActive }) => (
-                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 mt-2 rounded-full'>
+                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 rounded-full'>
                                     <AiFillHome className={`text-3xl ${isActive ? "block": "hidden"}`}/>
                                     <AiOutlineHome className={`text-3xl ${isActive ? "hidden": "block"}`}/>
-                                    <span className='ml-5 text-xl xl:block hidden'>ホーム</span>
+                                    <span className={`ml-5 text-xl xl:block hidden ${isActive ? "font-bold" : ""}`}>ホーム</span>
                                 </div>
                             )}
                         </NavLink>
 
-                        <NavLink to='/search' className={({ isActive }) => `${isActive ? "font-bold" : ""}`}>
+                        <NavLink to='/search' className="block mt-2 rounded-full" onClick={() => window.scrollTo(0, 0)}>
                             {({ isActive }) => (
-                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 mt-2 rounded-full'>
+                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 rounded-full'>
                                     <HiSearch className={`text-3xl ${isActive ? "block": "hidden"}`}/>
                                     <HiOutlineSearch className={`text-3xl ${isActive ? "hidden": "block"}`}/>
-                                    <span className='ml-5 text-xl xl:block hidden'>検索</span>
+                                    <span className={`ml-5 text-xl xl:block hidden ${isActive ? "font-bold" : ""}`}>検索</span>
                                 </div>
                             )}
                         </NavLink>
 
-                        <NavLink to='/sign-in' end className={({ isActive }) => `${isActive ? "" : ""}`}>
+                        <NavLink to='/sign-in' end className="block mt-2 rounded-full" onClick={() => window.scrollTo(0, 0)}>
                             {({ isActive }) => (
-                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 mt-2 rounded-full'>
+                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 rounded-full'>
                                     <AiOutlineLogin className={`text-3xl`}/>
-                                    <span className='ml-5 text-xl xl:block hidden'>サインイン</span>
+                                    <span className={`ml-5 text-xl xl:block hidden ${isActive ? "font-bold" : ""}`}>サインイン</span>
                                 </div>
                             )}
                         </NavLink>
