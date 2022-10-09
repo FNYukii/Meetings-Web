cd3ff75c97851bc1276b8db084ab344385bc1e33
Yu357 <forestgreen14689@gmail.com>
2022-10-09 19:38:16 +0900

    feat: RecentTagMenuを配置

---
+++ src/components/parts/RecentTagMenu.tsx
@@ -0,0 +1,33 @@
+import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu'
+import { VscEllipsis } from 'react-icons/vsc'
+import '@szhsin/react-menu/dist/index.css'
+import "@szhsin/react-menu/dist/theme-dark.css"
+import { useState } from "react"
+
+export default function RecentTagMenu(props: { tag: string }) {
+
+    const [isDark, setIsDark] = useState(false)
+
+    function checkTheme() {
+        const isDark = matchMedia(
+            '(prefers-color-scheme: dark)'
+        ).matches;
+
+        setIsDark(isDark)
+    }
+
+    const menuButton = (
+        <MenuButton className="hover:bg-zinc-100 dark:hover:bg-zinc-800 rounded-full aspect-square flex items-center p-1" onClick={checkTheme}>
+            <VscEllipsis className="text-xl text-gray-500" />
+        </MenuButton>
+    )
+
+    return (
+        <div>
+
+            <Menu menuButton={menuButton} theming={isDark ? "dark" : undefined}>
+                <MenuItem>興味なし</MenuItem>
+            </Menu>
+        </div>
+    )
+}
--- src/components/parts/RecentTagRow.tsx
+++ src/components/parts/RecentTagRow.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import FireThread from "../../utilities/FireThread"
+import RecentTagMenu from "./RecentTagMenu"
 
 export default function RecentTagRow(props: { tag: string }) {
 
@@ -19,7 +20,13 @@ export default function RecentTagRow(props: { tag: string }) {
 
     return (
         <div>
-            <p>{props.tag}</p>
+
+            <div className="flex justify-between">
+                <span>{props.tag}</span>
+
+                <RecentTagMenu tag={props.tag}/>
+
+            </div>
 
             {!isLoaded &&
                 <div className="bg-zinc-100 dark:bg-zinc-800 w-20">
