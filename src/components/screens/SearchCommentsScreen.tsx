9cc3d71ae8a22cf85252f1df48294f42c8d0bfd4
Yu357 <forestgreen14689@gmail.com>
2022-10-10 22:14:35 +0900

    feat: コメントを検索した

--- src/components/screens/SearchCommentsScreen.tsx
+++ src/components/screens/SearchCommentsScreen.tsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from "react"
+import Comment from "../../types/Comment"
+import FireComment from "../../utilities/FireComment"
+import progress from "../../images/progress.svg"
+import CommentRow from "../parts/CommentRow"
+
 export default function SearchCommentsScreen(props: {keyword: string, className?: string}) {
     
+    const [comments, setComments] = useState<Comment[] | null>(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    async function readThreads() {
+
+        const threads = await FireComment.readCommentsByKeyword(props.keyword)
+        setComments(threads)
+        setIsLoaded(true)
+    }
+
+    useEffect(() => {
+
+        readThreads()
+        // eslint-disable-next-line
+    }, [props.keyword])
+
     return (
         <div className={props.className}>
-            <p>Search comments by {props.keyword}</p>
+
+            {!isLoaded &&
+                <div className='flex justify-center p-3'>
+                    <img src={progress} alt='loading' />
+                </div>
+            }
+
+            {isLoaded && comments === null &&
+                <div className="p-3">
+                    <p className="text-gray-500 text-center">読み取りに失敗しました。</p>
+                </div>
+            }
+
+            {isLoaded && comments !== null && comments.length === 0 &&
+                <div className="p-3">
+                    <p className="text-gray-500 text-center">結果なし</p>
+                </div>
+            }
+
+            {isLoaded && comments !== null &&
+                <div>
+                    {comments.map((comment) => (
+                        <CommentRow key={comment.id} comment={comment}/>
+                    ))}
+                </div>
+            }
         </div>
     )
 }
