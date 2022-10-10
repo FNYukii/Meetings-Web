03fd41cf96047d7b54c8a6c611e2de310e26bb77
Yu357 <forestgreen14689@gmail.com>
2022-10-10 21:32:30 +0900

    feat: SearchedScreenにタブバーを配置

--- src/components/screens/SearchedScreen.tsx
+++ src/components/screens/SearchedScreen.tsx
@@ -1,6 +1,53 @@
-export default function SearchedScreen(props: {keyword: string}) {
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+import { useLocation } from "react-router-dom"
+
+export default function SearchedScreen(props: { keyword: string }) {
+
+    const [tab, setTab] = useState(0)
+
     return (
-        <div className="p-3">
+        <div>
+
+            <div className="mt-3 flex border-b border-zinc-200 dark:border-zinc-800">
+
+                <button onClick={() => setTab(0)} className="w-1/3 hover:bg-zinc-100 dark:hover:bg-zinc-900 relative">
+
+                    <div className="text-center p-3">
+
+                        <span className={tab === 0 ? "font-bold" : ""}>スレッド</span>
+
+                        <div className="absolute bottom-0 left-0 w-full">
+                            <div className={`h-0.5 mx-3 ${tab === 0 ? "bg-black dark:bg-white" : ""}`}></div>
+                        </div>
+                    </div>
+                </button>
+
+                <button onClick={() => setTab(1)} className="w-1/3 hover:bg-zinc-100 dark:hover:bg-zinc-900 relative">
+
+                    <div className="text-center p-3">
+
+                        <span className={tab === 1 ? "font-bold" : ""}>コメント</span>
+
+                        <div className="absolute bottom-0 left-0 w-full">
+                            <div className={`h-0.5 mx-3 ${tab === 1 ? "bg-black dark:bg-white" : ""}`}></div>
+                        </div>
+                    </div>
+                </button>
+
+                <button onClick={() => setTab(2)} className="w-1/3 hover:bg-zinc-100 dark:hover:bg-zinc-900 relative">
+
+                    <div className="text-center p-3">
+
+                        <span className={tab === 2 ? "font-bold" : ""}>ユーザー</span>
+
+                        <div className="absolute bottom-0 left-0 w-full">
+                            <div className={`h-0.5 mx-3 ${tab === 2 ? "bg-black dark:bg-white" : ""}`}></div>
+                        </div>
+                    </div>
+                </button>
+            </div>
+
             <p>{props.keyword} を検索</p>
         </div>
     )
