60cf2a79b4132fe97a6b8a5f9c6f7cbc99ef6f0b
Yu357 <forestgreen14689@gmail.com>
2022-10-10 13:31:49 +0900

    feat: 画像付きのコメントを5件表示

--- src/components/screens/RecentImagesScreen.tsx
+++ src/components/screens/RecentImagesScreen.tsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react"
+import Comment from "../../types/Comment"
+import FireComment from "../../utilities/FireComment"
+import progress from "../../images/progress.svg"
+import CommentRow from "../parts/CommentRow"
+
 export default function RecentImagesScreen() {
+
+    const [comments, setComments] = useState<Comment[] | null>(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    async function readComments() {
+
+        const comments = await FireComment.readCommentsWithImages()
+        setComments(comments)
+        setIsLoaded(true)
+    }
+
+    useEffect(() => {
+
+        readComments()
+    }, [])
+
     return (
-        <div></div>
+        <div>
+            {!isLoaded &&
+                <div className='flex justify-center p-3'>
+                    <img src={progress} alt='loading' />
+                </div>
+            }
+
+            {isLoaded && comments === null &&
+                <div className="p-2">
+                    <p className="text-gray-500 text-center">読み取りに失敗しました。</p>
+                </div>
+            }
+
+            {isLoaded && comments !== null &&
+                <div>
+                    {comments.map((comment) => (
+                        <CommentRow key={comment.id} comment={comment} isShowThreadTitle={false}/>
+                    ))}
+                </div>
+            }
+        </div>
     )
 }
