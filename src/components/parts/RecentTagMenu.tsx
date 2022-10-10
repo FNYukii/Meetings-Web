8676a819e152f34d63d0b318bd496f99c301df24
Yu357 <forestgreen14689@gmail.com>
2022-10-11 01:50:25 +0900

    fix: RecentTagMenuのボタンがMenuより前面に表示される不具合を修正

--- src/components/parts/RecentTagMenu.tsx
+++ src/components/parts/RecentTagMenu.tsx
@@ -18,13 +18,12 @@ export default function RecentTagMenu(props: { tag: string }) {
 
     const menuButton = (
         <MenuButton className="hover:bg-zinc-200 dark:hover:bg-zinc-700 rounded-full aspect-square flex items-center p-1" onClick={checkTheme}>
-            <VscEllipsis className="text-xl text-gray-500" />
+            <VscEllipsis className="z-10 text-xl text-gray-500" />
         </MenuButton>
     )
 
     return (
-        <div className='z-10'>
-
+        <div>
             <Menu menuButton={menuButton} theming={isDark ? "dark" : undefined}>
                 <MenuItem>興味なし</MenuItem>
             </Menu>
