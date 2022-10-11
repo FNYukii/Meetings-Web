13b5367e6202fa5bc273b3ec63cbe06df6b8a573
Yu357 <forestgreen14689@gmail.com>
2022-10-11 19:04:06 +0900

    feat: Routesに背景ページを設定できた

--- src/components/columns/CenterColumn.tsx
+++ src/components/columns/CenterColumn.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation, useRoutes } from 'react-router-dom'
 
 import HomeScreen from '../screens/HomeScreen'
 import ThreadScreen from '../screens/ThreadScreen'
@@ -6,21 +6,41 @@ import NotFoundScreen from '../screens/NotFoundScreen'
 import SearchScreen from '../screens/SearchScreen'
 import CommentScreen from '../screens/CommentScreen'
 import UserScreen from '../screens/UserScreen'
+import ImageModal from '../modals/ImageModal'
 
 export default function CenterColumn() {
+
+    const location = useLocation()
+    const state = location.state as { backgroundPath?: string }
+    const background = state?.backgroundPath ?? '/'
+
+    const modalElement = useRoutes([
+        {
+            path: '/images/:imageUrl',
+            element: <ImageModal />
+        }
+    ])
+
+    console.log(`modalElement ${modalElement}`)
+    console.log(`background ${background}`)
+
 
     return (
         <div className='xl:w-2/4 md:w-7/12 w-full min-h-screen border-l border-r border-zinc-200 dark:border-zinc-800'>
 
-            <Routes>
-                
+            <Routes location={modalElement !== null ? background : undefined}>
+
                 <Route path='/' element={<HomeScreen />} />
                 <Route path='/search' element={<SearchScreen />} />
                 <Route path='/threads/:threadId' element={<ThreadScreen />} />
                 <Route path='/comments/:commentId' element={<CommentScreen />} />
-                <Route path='/users/:userId' element={<UserScreen />}/>
+                <Route path='/users/:userId' element={<UserScreen />} />
                 <Route path='*' element={<NotFoundScreen />} />
             </Routes>
+
+            {modalElement !== null &&
+                <ImageModal/>
+            }
         </div>
     )
 }
