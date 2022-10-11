f2a13118d6f3992967afeb370f88c464b472f8c4
Yu357 <forestgreen14689@gmail.com>
2022-10-11 21:39:47 +0900

    feat: 画像を表示した

--- src/components/modals/ImageModal.tsx
+++ src/components/modals/ImageModal.tsx
@@ -1,21 +1,46 @@
+import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import Comment from "../../types/Comment"
+import FireComment from "../../utilities/FireComment"
+import progress from "../../images/progress.svg"
 
-export default function ImageModal(props: {className?: string}) {
+export default function ImageModal(props: { className?: string }) {
 
     const navigate = useNavigate()
     const { commentId, imageIndex } = useParams()
 
+    const [comment, setComment] = useState<Comment | null>(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    async function readComment() {
+        const comment = await FireComment.readCommentFromCache(commentId!)
+        setComment(comment)
+        setIsLoaded(true)
+    }
+
+    useEffect(() => {
+        readComment()
+        // eslint-disable-next-line
+    }, [])
+
     return (
         <div className={`${props.className} z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center`}>
 
-            <div className="w-full h-full bg-black/30 dark:bg-white/20" onClick={() => navigate(-1)}></div>
+            <div className="w-full h-full bg-black/80" onClick={() => navigate(-1)}></div>
 
             <div className="absolute">
-                <div className="w-96 aspect-video p-3 bg-white dark:bg-black rounded-xl">
-                    <h1>Modal</h1>
-                    <p>commentId: {commentId!}</p>
-                    <p>imageIndex: {imageIndex!}</p>
-                </div>
+
+                {!isLoaded &&
+                    <img src={progress} alt='loading' />
+                }
+
+                {isLoaded && comment === null &&
+                    <p className="text-gray-500 text-center">読み取りに失敗しました。</p>
+                }
+
+                {isLoaded && comment !== null &&
+                    <img src={comment.imageUrls[parseInt(imageIndex!)]} alt="" />
+                }
             </div>
         </div>
     )
