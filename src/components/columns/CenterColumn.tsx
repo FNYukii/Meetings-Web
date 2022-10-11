a37ada29ae1895751f48dad484fb96c53f9affcb
Yu357 <forestgreen14689@gmail.com>
2022-10-11 20:55:23 +0900

    feat: 初期値の設定を取り消し

--- src/components/columns/CenterColumn.tsx
+++ src/components/columns/CenterColumn.tsx
@@ -12,7 +12,7 @@ export default function CenterColumn() {
 
     const location = useLocation()
     const state = location.state as { previousPath?: string }
-    const previousPath: string | undefined = state?.previousPath ?? "/"
+    const previousPath: string | undefined = state?.previousPath
 
     return (
         <div className='xl:w-2/4 md:w-7/12 w-full min-h-screen border-l border-r border-zinc-200 dark:border-zinc-800'>
