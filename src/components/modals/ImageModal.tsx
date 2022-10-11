3baa5ef739537216475142f147477c40a45896f1
Yu357 <forestgreen14689@gmail.com>
2022-10-11 17:46:00 +0900

    feat: ImageModalの影をダークテーマに対応

--- src/components/modals/ImageModal.tsx
+++ src/components/modals/ImageModal.tsx
@@ -2,7 +2,7 @@ export default function ImageModal(props: {className?: string}) {
 
     return (
         <div className={props.className}>
-            <div className="z-30 fixed top-0 left-0 w-full h-full bg-black/30"></div>
+            <div className="z-30 fixed top-0 left-0 w-full h-full bg-black/30 dark:bg-white/20"></div>
         </div>
     )
 }
