dc3f2bf032c093f9ec50530137f7b668e8dd7e1c
Yu357 <forestgreen14689@gmail.com>
2022-10-12 12:49:28 +0900

    feat: CommentImageNavLinkのhover時の背景色を調整

--- src/components/parts/CommentImageNavLink.tsx
+++ src/components/parts/CommentImageNavLink.tsx
@@ -6,8 +6,10 @@ export default function CommentImageNavLink(props: { comment: Comment, imageInde
     const location = useLocation()
 
     return (
-        <Link to={`/comments/${props.comment.id}/images/${props.imageIndex + 1}`} state={{ previousPath: location.pathname }} className="hover:opacity-80 pointer-events-auto">
+        <Link to={`/comments/${props.comment.id}/images/${props.imageIndex + 1}`} state={{ previousPath: location.pathname }} className="pointer-events-auto relative">
+
             <img src={props.comment.imageUrls[props.imageIndex]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
+            <div className="absolute top-0 left-0 hover:bg-black/10 dark:hover:bg-white/10 w-full h-full rounded-xl"></div>
         </Link>
     )
 }
