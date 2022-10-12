526c0d87000997efba4e37119c9fa78717c3ebe4
Yu357 <forestgreen14689@gmail.com>
2022-10-12 23:47:32 +0900

    feat: タイトルを表示

--- src/components/modals/ReportModal.tsx
+++ src/components/modals/ReportModal.tsx
@@ -11,7 +11,7 @@ export default function ReportModal(props: { className?: string }) {
 
     function closeModal() {
         body.style.overflowY = ""
-        
+
         navigate(-1)
     }
 
@@ -20,8 +20,27 @@ export default function ReportModal(props: { className?: string }) {
 
             <div className="w-full h-full bg-black/20 dark:bg-white/20" onClick={closeModal}></div>
 
-            <div className="absolute bg-white p-3">
-                <h1>Report</h1>
+            <div className="absolute bg-white p-6 rounded-xl">
+
+                <p className="text-xl">
+
+                    <span>
+                        {collectionName === "threads" &&
+                            <span>スレッド</span>
+                        }
+
+                        {collectionName === "commments" &&
+                            <span>コメント</span>
+                        }
+
+                        {collectionName === "users" &&
+                            <span>ユーザー</span>
+                        }
+                    </span>
+
+                    <span>を報告</span>
+                </p>
+
                 <p>collection: {collectionName}</p>
                 <p>documentId: {documentId}</p>
             </div>
