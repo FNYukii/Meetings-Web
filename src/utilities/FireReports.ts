00c84838ab06bed815b93c290778f5e04a3d19f2
Yu357 <forestgreen14689@gmail.com>
2022-10-13 21:35:09 +0900

    feat: FireReportsファイルを作成

---
+++ src/utilities/FireReports.ts
@@ -0,0 +1,6 @@
+export default class FireReports {
+
+    static async createReport(targetId: string, targetCollectionId: string, probremCategory: number, detail: string): Promise<string | null> {
+        return null
+    }
+}
