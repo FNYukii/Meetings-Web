fe783070fa161c7cfb7f438365cb039a89fad5fb
Yu357 <forestgreen14689@gmail.com>
2022-10-13 11:04:56 +0900

    feat: 閉じるボタンを配置

--- src/components/modals/ReportModal.tsx
+++ src/components/modals/ReportModal.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { MdOutlineClose } from "react-icons/md"
 import { useNavigate, useParams } from "react-router-dom"
 
 export default function ReportModal(props: { className?: string }) {
@@ -24,9 +25,13 @@ export default function ReportModal(props: { className?: string }) {
 
             <div className="w-full h-full bg-black/20 dark:bg-white/20" onClick={closeModal}></div>
 
-            <div className="absolute bg-white p-6 rounded-xl width-600">
+            <div className="absolute bg-white dark:bg-black pt-3 pb-6 pl-3 pr-6 rounded-xl width-600">
 
-                <h2 className="text-2xl font-bold">
+                <button onClick={closeModal} className="p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
+                    <MdOutlineClose className="text-2xl text-gray-500" />
+                </button>
+
+                <h2 className="text-2xl font-bold ml-3">
 
                     <span>
                         {collectionName === "threads" &&
@@ -45,34 +50,34 @@ export default function ReportModal(props: { className?: string }) {
                     <span>を報告</span>
                 </h2>
 
-                <fieldset className="mt-3 flex gap-2 flex-col">
+                <fieldset className="mt-3 ml-3 flex gap-2 flex-col">
                     <legend className="text-xl">カテゴリ</legend>
 
                     <div className="mt-1 ml-1">
-                        <input type="radio" id="radio01" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(0)}/>
+                        <input type="radio" id="radio01" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(0)} />
                         <label htmlFor="radio01" className="pl-3 cursor-pointer">暴力的</label>
                     </div>
 
                     <div className="ml-1">
-                        <input type="radio" id="radio02" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(1)}/>
+                        <input type="radio" id="radio02" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(1)} />
                         <label htmlFor="radio02" className="pl-3 cursor-pointer">センシティブ</label>
                     </div>
 
                     <div className="ml-1">
-                        <input type="radio" id="radio03" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(2)}/>
+                        <input type="radio" id="radio03" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(2)} />
                         <label htmlFor="radio03" className="pl-3 cursor-pointer">スパム</label>
                     </div>
 
                     <div className="ml-1">
-                        <input type="radio" id="radio04" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(3)}/>
+                        <input type="radio" id="radio04" name="category" className="p-3 cursor-pointer scale-125" onChange={() => setRadioSelection(3)} />
                         <label htmlFor="radio04" className="pl-3 cursor-pointer">虚偽</label>
                     </div>
                 </fieldset>
 
-                <fieldset className="mt-3">
+                <fieldset className="mt-3 ml-3">
                     <legend className="text-xl">詳細</legend>
 
-                    <textarea placeholder="報告する理由を詳しく入力してください" className="resize-none mt-1 p-3 border rounded-md border-zinc-500 w-full"/>
+                    <textarea placeholder="報告する理由を詳しく入力してください" className="resize-none mt-1 p-3 border rounded-md border-zinc-500 w-full" />
                 </fieldset>
 
 
