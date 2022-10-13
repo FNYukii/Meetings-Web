9dd5a1bbb39dd1bba4314d2d63d9e8390254fef4
Yu357 <forestgreen14689@gmail.com>
2022-10-13 21:41:15 +0900

    feat: reportsコレクションに新しいドキュメントを追加するメソッドを作成

--- src/utilities/FireReports.ts
+++ src/utilities/FireReports.ts
@@ -1,6 +1,27 @@
+import { addDoc, collection, FieldValue, serverTimestamp } from "firebase/firestore"
+import { db } from "./firebase"
+
 export default class FireReports {
 
     static async createReport(targetId: string, targetCollectionId: string, probremCategory: number, detail: string): Promise<string | null> {
-        return null
+
+        try {
+
+            const docRef = await addDoc(collection(db, "reports"), {
+                createdAt: serverTimestamp(),
+                targetId: targetId,
+                targetCollectionId: targetCollectionId,
+                probremCategory: probremCategory,
+                detail: detail
+            })
+            
+            console.log("Add 1 Report.")
+
+            return docRef.id
+
+        } catch (error) {
+
+            return null
+        }
     }
 }
