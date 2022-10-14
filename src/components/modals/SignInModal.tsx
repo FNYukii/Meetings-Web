fd853475cc2218179fcaf1b2710abe322463d416
Yu357 <forestgreen14689@gmail.com>
2022-10-14 17:17:34 +0900

    feat: 閉じるボタンと送信ボタンを配置

--- src/components/modals/SignInModal.tsx
+++ src/components/modals/SignInModal.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import { useEffect } from "react"
+import { MdOutlineClose } from "react-icons/md"
 
 
 export default function SignInModal() {
@@ -19,11 +20,23 @@ export default function SignInModal() {
 
     return (
         <div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
-            
+
             <div onClick={closeModal} className="w-full h-full bg-black/20 dark:bg-white/20"></div>
 
             <div className="absolute bg-white dark:bg-black p-6 rounded-xl md:width-600 w-11/12">
-                <p>hello</p>
+
+                <button onClick={closeModal} className="p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
+                    <MdOutlineClose className="text-2xl text-gray-500" />
+                </button>
+
+                <div className="px-3">
+
+                    <h2 className="text-2xl font-bold">サインイン</h2>
+                </div>
+
+                <div className="mt-3 flex justify-end">
+                    <button className={`font-bold p-3 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 $`}>送信</button>
+                </div>
             </div>
         </div>
     )
