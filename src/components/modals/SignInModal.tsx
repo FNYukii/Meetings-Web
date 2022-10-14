136b4f0db3d3599bfe050d06e8b2719cb3ad07a6
Yu357 <forestgreen14689@gmail.com>
2022-10-14 16:58:55 +0900

    feat: SignInModalを表示

---
+++ src/components/modals/SignInModal.tsx
@@ -0,0 +1,14 @@
+export default function SignInModal() {
+
+    return (
+        <div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
+            
+            <div className="w-full h-full bg-black/20 dark:bg-white/20"></div>
+
+            <div className="absolute bg-white dark:bg-black p-6 rounded-xl md:width-600 w-11/12">
+
+                <p>hello</p>
+            </div>
+        </div>
+    )
+}
