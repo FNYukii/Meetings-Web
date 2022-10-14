27cf2033af68740016314bcf8269a7b77cc0b179
Yu357 <forestgreen14689@gmail.com>
2022-10-14 21:35:18 +0900

    feat: サインイン後はGlobal Navにプロフィールボタンが現れるようになった

--- src/components/columns/LeftColumn.tsx
+++ src/components/columns/LeftColumn.tsx
@@ -1,10 +1,29 @@
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import { AiOutlineHome, AiFillHome, AiOutlineLogin } from 'react-icons/ai'
 import { HiSearch, HiOutlineSearch } from "react-icons/hi"
+import { useEffect, useState } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export default function LeftColumn(props: { className?: string }) {
 
     const location = useLocation()
+    const [uid, setUid] = useState<string | null>(null)
+
+    async function listenUid() {
+
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUid(user.uid)
+            } else {
+                setUid(null)
+            }
+        })
+    }
+
+    useEffect(() => {
+        listenUid()
+    }, [])
 
     return (
         <div className={props.className}>
@@ -43,14 +62,31 @@ export default function LeftColumn(props: { className?: string }) {
                             )}
                         </NavLink>
 
-                        <Link to='/sign-in' state={{ previousPath: location.pathname }} className="block mt-2 rounded-full">
+                        {uid === null &&
+                            <Link to='/sign-in' state={{ previousPath: location.pathname }} className="block mt-2 rounded-full">
 
-                            <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 rounded-full'>
+                                <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 rounded-full'>
 
-                                <AiOutlineLogin className={`text-3xl`} />
-                                <span className={`ml-5 text-xl xl:block hidden`}>サインイン</span>
-                            </div>
-                        </Link>
+                                    <AiOutlineLogin className={`text-3xl`} />
+                                    <span className={`ml-5 text-xl xl:block hidden`}>サインイン</span>
+                                </div>
+                            </Link>
+                        }
+
+                        {uid !== null &&
+                            <NavLink to={`/users/${uid}`} className="block mt-2 rounded-full" onClick={() => window.scrollTo(0, 0)}>
+
+                                {({ isActive }) => (
+
+                                    <div className='flex items-center hover:bg-zinc-100 dark:hover:bg-zinc-900 p-2 rounded-full'>
+
+                                        <HiSearch className={`text-3xl ${isActive ? "block" : "hidden"}`} />
+                                        <HiOutlineSearch className={`text-3xl ${isActive ? "hidden" : "block"}`} />
+                                        <span className={`ml-5 text-xl xl:block hidden ${isActive ? "font-bold" : ""}`}>プロフィール</span>
+                                    </div>
+                                )}
+                            </NavLink>
+                        }
                     </div>
                 </div>
             </div>
--- src/components/columns/RightColumn.tsx
+++ src/components/columns/RightColumn.tsx
@@ -1,4 +1,3 @@
-import FireAuth from "../../utilities/FireAuth";
 import RecentTagsCard from "../parts/RecentTagsCard";
 
 export default function RightColumn(props: {className?: string}) {
@@ -16,8 +15,6 @@ export default function RightColumn(props: {className?: string}) {
                     
                     <a href="https://github.com/Yu357/Meetings-Web" target="blank" className="hover:underline">Source</a>
                 </div>
-
-                <p>{FireAuth.uid ()?? "null"}</p>
             </div>
         </div>
     )
