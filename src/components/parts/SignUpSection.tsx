e910b570647c2a208ffea64bd211034efa11d748
Yu357 <forestgreen14689@gmail.com>
2022-10-15 15:56:59 +0900

    feat: inputを配置

--- src/components/parts/SignUpSection.tsx
+++ src/components/parts/SignUpSection.tsx
@@ -6,6 +6,9 @@ export default function SignUpSection(props: {setIsShowSignUpSection: React.Disp
     const [password, setPassword] = useState("")
     const [password2, setPassword2] = useState("")
 
+    const [displayName, setDisplayName] = useState("")
+    const [userTag, setUserTag] = useState("")
+
     return (
         <div>
 
@@ -17,6 +20,9 @@ export default function SignUpSection(props: {setIsShowSignUpSection: React.Disp
 
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="パスワード" className="mt-5 p-2 w-full rounded-md border border-gray-500 bg-transparent placeholder:text-gray-500" />
                 <input type="password" value={password2} onChange={(e) => setPassword2(e.target.value)} placeholder="パスワードを確認" className="mt-5 p-2 w-full rounded-md border border-gray-500 bg-transparent placeholder:text-gray-500" />
+
+                <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} placeholder="ディスプレイネーム" className="mt-10 p-2 w-full rounded-md border border-gray-500 bg-transparent placeholder:text-gray-500" />
+                <input type="text" value={userTag} onChange={(e) => setUserTag(e.target.value)} placeholder="ユーザータグ" className="mt-5 p-2 w-full rounded-md border border-gray-500 bg-transparent placeholder:text-gray-500" />
             </div>
 
             <div className="mt-3 pl-3 flex justify-between items-center">
