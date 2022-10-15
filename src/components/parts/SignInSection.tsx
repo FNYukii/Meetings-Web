3243ad384a38005b333214a850d57e16533cadaa
Yu357 <forestgreen14689@gmail.com>
2022-10-15 15:45:22 +0900

    feat: テキスト調整

--- src/components/parts/SignInSection.tsx
+++ src/components/parts/SignInSection.tsx
@@ -42,9 +42,9 @@ export default function SignInSection(props: {setIsShowSignUpSection: React.Disp
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="パスワード" className="mt-5 p-2 w-full rounded-md border border-gray-500 bg-transparent placeholder:text-gray-500" />
             </div>
 
-            <div className="mt-3 ml-3 flex justify-between">
+            <div className="mt-3 pl-3 flex justify-between items-center">
 
-                <button onClick={() => props.setIsShowSignUpSection(true)} className="hover:underline h-fit">サインアップ</button>
+                <button onClick={() => props.setIsShowSignUpSection(true)} className="hover:underline h-fit">新しいアカウントを作成</button>
                 <button onClick={signIn} disabled={email === "" || password === ""} className={`font-bold p-3 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 ${email === "" || password === "" ? "text-gray-400 dark:text-gray-600 hover:bg-transparent dark:hover:bg-transparent" : ""}`}>サインイン</button>
             </div>
         </div>
--- src/components/parts/SignUpSection.tsx
+++ src/components/parts/SignUpSection.tsx
@@ -11,7 +11,7 @@ export default function SignUpSection(props: {setIsShowSignUpSection: React.Disp
 
             <div className="px-3 mt-3">
 
-                <h2 className="text-2xl font-bold">サインアップ</h2>
+                <h2 className="text-2xl font-bold">アカウントを作成</h2>
 
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="メールアドレス" className="mt-5 p-2 w-full rounded-md border border-gray-500 bg-transparent placeholder:text-gray-500" />
 
@@ -19,9 +19,9 @@ export default function SignUpSection(props: {setIsShowSignUpSection: React.Disp
                 <input type="password" value={password2} onChange={(e) => setPassword2(e.target.value)} placeholder="パスワードを確認" className="mt-5 p-2 w-full rounded-md border border-gray-500 bg-transparent placeholder:text-gray-500" />
             </div>
 
-            <div className="mt-3 ml-3 flex justify-between">
+            <div className="mt-3 pl-3 flex justify-between items-center">
 
-                <button onClick={() => props.setIsShowSignUpSection(false)} className="hover:underline h-fit">サインイン</button>
+                <button onClick={() => props.setIsShowSignUpSection(false)} className="hover:underline h-fit">既存のアカウントでサインイン</button>
                 <button disabled={email === "" || password === ""} className={`font-bold p-3 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 ${email === "" || password === "" ? "text-gray-400 dark:text-gray-600 hover:bg-transparent dark:hover:bg-transparent" : ""}`}>サインアップ</button>
             </div>
         </div>
