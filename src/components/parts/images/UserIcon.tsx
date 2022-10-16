a31ce4d6c8fd60ec739b853b4b8762ad7eff9f33
Yu357 <forestgreen14689@gmail.com>
2022-10-16 11:35:04 +0900

    fix: UserIconを修正

--- src/components/parts/images/UserIcon.tsx
+++ src/components/parts/images/UserIcon.tsx
@@ -1,10 +1,12 @@
-export default function UserIcon(props: {iconUrl: string, className?: string}) {
+export default function UserIcon(props: { iconUrl: string | null, className?: string }) {
     return (
         <div className={`relative aspect-square ${props.className}`}>
 
             <div className="bg-zinc-200 dark:bg-zinc-800 rounded-full absolute top-0 left-0 w-full h-full"></div>
-            
-            <img className="rounded-full absolute top-0 left-0 w-full h-full" src={props.iconUrl} alt="" />
+
+            {props.iconUrl &&
+                <img className="rounded-full absolute top-0 left-0 w-full h-full" src={props.iconUrl} alt="" />
+            }
         </div>
     )
 }
