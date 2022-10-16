9c7239821107e517fef0825db411a057f684472e
Yu357 <forestgreen14689@gmail.com>
2022-10-16 13:16:54 +0900

    feat: NewCommentLinkを切り出し

---
+++ src/components/parts/links/NewCommentLink.tsx
@@ -0,0 +1,43 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { Link, useLocation } from "react-router-dom";
+import FireAuth from "../../../utilities/FireAuth";
+import { auth } from "../../../utilities/firebase";
+
+export default function NewCommentLink(props: { threadId: string }) {
+
+    const location = useLocation()
+    const [uid, setUid] = useState<string | null>(FireAuth.uidFromLocalStorage())
+
+    useEffect(() => {
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+
+                const uid = user.uid
+                setUid(uid)
+
+            } else {
+
+                setUid(null)
+            }
+        })
+    }, [])
+
+    return (
+        <div className="z-10">
+
+            {uid !== null &&
+
+                <Link to={`/threads/${props.threadId}/new`} state={{ previousPath: location.pathname }}>
+
+                    <div className="p-2 rounded-full hover:bg-zinc-100 dark:hover:bg-zinc-900">
+
+                        <AiOutlinePlus className="text-2xl" />
+                    </div>
+                </Link>
+            }
+        </div>
+    )
+}
--- src/components/screens/ThreadScreen.tsx
+++ src/components/screens/ThreadScreen.tsx
@@ -1,12 +1,11 @@
-import { Link, useLocation, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import BackButton from "../parts/buttons/BackButton"
-import { AiOutlinePlus } from "react-icons/ai"
 import CommentsInThreadList from "../parts/lists/CommentsInThreadList"
 import ThreadTitleSpan from "../parts/spans/ThreadTitleSpan"
+import NewCommentLink from "../parts/links/NewCommentLink"
 
 export default function ThreadScreen() {
 
-    const location = useLocation()
     const { threadId } = useParams()
 
     return (
@@ -22,9 +21,7 @@ export default function ThreadScreen() {
                         <ThreadTitleSpan threadId={threadId!} />
                     </div>
 
-                    <Link to={`/threads/${threadId}/new`} state={{ previousPath: location.pathname }} className="z-10 p-2 rounded-full hover:bg-zinc-100 dark:hover:bg-zinc-900">
-                        <AiOutlinePlus className="text-2xl" />
-                    </Link>
+                    <NewCommentLink threadId={threadId!} />
                 </div>
             </div>
 
