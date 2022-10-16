ba5cc4381359daa562ecd0ba07783f480d74e1af
Yu357 <forestgreen14689@gmail.com>
2022-10-16 19:23:34 +0900

    clean

--- src/components/modals/AddThreadModal.tsx
+++ src/components/modals/AddThreadModal.tsx
@@ -26,15 +26,10 @@ export default function AddThreadModal() {
 
     })
 
-    function closeModal() {
-
-        navigate(-1)
-    }
-
     function onKeyDown(event: KeyboardEvent) {
 
         if (event.key === "Escape") {
-            closeModal()
+            navigate(-1)
         }
     }
 
@@ -57,17 +52,17 @@ export default function AddThreadModal() {
         // 成功
         // コメントを作成
         await FireComments.createComment(threadId, text, [])
-        closeModal()
+        navigate(-1)
     }
 
     return (
         <div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
 
-            <div onClick={closeModal} className="w-full h-full bg-black/20 dark:bg-white/20"></div>
+            <div onClick={() => navigate(-1)} className="w-full h-full bg-black/20 dark:bg-white/20"></div>
 
             <div className="absolute bg-white dark:bg-black p-6 rounded-xl md:width-600 w-11/12">
 
-                <button onClick={closeModal} className="p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
+                <button onClick={() => navigate(-1)} className="p-3 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full">
                     <MdOutlineClose className="text-2xl text-gray-500" />
                 </button>
 
