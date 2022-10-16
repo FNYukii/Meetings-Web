e149bff97bea51f08ba14f21cef7c89dd6c7c48e
Yu357 <forestgreen14689@gmail.com>
2022-10-16 12:59:10 +0900

    feat: サインイン状態によって、NewThreadLinkの表示を切り替えた

---
+++ src/components/parts/links/NewThreadLink.tsx
@@ -0,0 +1,36 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import FireAuth from "../../../utilities/FireAuth";
+import { auth } from "../../../utilities/firebase";
+
+export default function NewThreadLink() {
+
+    const [uid, setUid] = useState<string | null>(FireAuth.uidFromLocalStorage())
+
+    useEffect(() => {
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+
+                const uid = user.uid
+                setUid(uid)
+
+            } else {
+
+                setUid(null)
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            {uid !== null &&
+                <Link to={`/new`} className="z-10 p-2 rounded-full hover:bg-zinc-100 dark:hover:bg-zinc-900">
+                    <AiOutlinePlus className="text-2xl" />
+                </Link>
+            }
+        </div>
+    )
+}
