79a64d8dbaa811303ef0633ba3995375f534b125
Yu357 <forestgreen14689@gmail.com>
2022-10-17 20:02:59 +0900

    feat: サインインしていない時はいいねボタンをdisabledにした

--- src/components/parts/buttons/CommentLikeButton.tsx
+++ src/components/parts/buttons/CommentLikeButton.tsx
@@ -1,13 +1,17 @@
+import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { AiOutlineExclamationCircle, AiOutlineHeart } from "react-icons/ai";
 import Comment from "../../../entities/Comment";
 import User from "../../../entities/User";
+import { auth } from "../../../utilities/firebase";
 import FireUsers from "../../../utilities/FireUsers";
 
 export default function CommentLikeButton(props: { comment: Comment, isReadFromSeaver: boolean, className?: string}) {
 
     const [likedUsers, setLikedUsers] = useState<User[] | null>(null)
     const [isLoaded, setIsLoaded] = useState(false)
+
+    const [uid, setUid] = useState<string | null>(null)
 
     async function readLikedUsers() {
         
@@ -24,15 +28,22 @@ export default function CommentLikeButton(props: { comment: Comment, isReadFromS
 
     useEffect(() => {
         readLikedUsers()
-        // eslint-disable-next-line
-    }, [])
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUid(user.uid)
+            } else {
+                setUid(null)
+            }
+        })
+    })
 
     return (
         <div className={props.className}>
             {!isLoaded &&
                 <div className="flex">
 
-                    <button className="z-10 flex items-center p-1 rounded-full pointer-events-auto hover:bg-zinc-100 dark:hover:bg-zinc-900" onClick={() => console.log("hello")}>
+                    <button disabled={true} className="z-10 flex items-center p-1 rounded-full pointer-events-auto hover:bg-zinc-100 dark:hover:bg-zinc-900">
                         <AiOutlineHeart className="text-xl text-gray-500 " />
                         <span className="text-gray-500 ml-1">0</span>
                     </button>
@@ -48,7 +59,7 @@ export default function CommentLikeButton(props: { comment: Comment, isReadFromS
             {isLoaded && likedUsers !== null &&
                 <div className="flex">
 
-                    <button className="z-10 flex items-center p-1 rounded-full pointer-events-auto hover:bg-zinc-100 dark:hover:bg-zinc-900" onClick={() => console.log("hello")}>
+                    <button disabled={uid === null} className={`z-10 flex items-center p-1 rounded-full pointer-events-auto ${uid === null ? "" : "hover:bg-zinc-100 dark:hover:bg-zinc-900"}`}>
                         <AiOutlineHeart className="text-xl text-gray-500 " />
                         <span className="text-gray-500 ml-1">{likedUsers?.length ?? "0"}</span>
                     </button>
