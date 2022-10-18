f4be993752904f865e88e01929fd7d8163d7f117
Yu357 <forestgreen14689@gmail.com>
2022-10-18 19:29:32 +0900

    feat: Enterキーを押すことで関数を実行できるようにした

--- src/components/modals/EditUserModal.tsx
+++ src/components/modals/EditUserModal.tsx
@@ -9,6 +9,12 @@ export default function EditUserModal() {
     const [userTag, setUserTag] = useState("")
     const [detail, setDetail] = useState("")
 
+    function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        alert("hello")
+    }
+
     return (
         <div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
 
@@ -20,17 +26,19 @@ export default function EditUserModal() {
                     <MdOutlineClose className="text-2xl text-gray-500" />
                 </button>
 
-                <div className="px-3 mt-3">
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <div className="px-3 mt-3">
 
-                    <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} placeholder="ディスプレイネーム" className="p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
-                    <input type="text" value={userTag} onChange={(e) => setUserTag(e.target.value)} placeholder="ユーザータグ" className="mt-5 p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
+                        <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} placeholder="ディスプレイネーム" className="p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
+                        <input type="text" value={userTag} onChange={(e) => setUserTag(e.target.value)} placeholder="ユーザータグ" className="mt-5 p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
 
-                    <textarea value={detail} onChange={(e) => setDetail(e.target.value)} placeholder="自己紹介" className="mt-5 h-24 resize-none p-3 border rounded-md border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500 w-full" />
-                </div>
+                        <textarea value={detail} onChange={(e) => setDetail(e.target.value)} placeholder="自己紹介" className="mt-5 h-24 resize-none p-3 border rounded-md border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500 w-full" />
+                    </div>
 
-                <div className="mt-3 flex justify-end">
-                    <button type="submit" disabled={displayName === "" || userTag === "" || detail === ""} className={`font-bold p-3 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 ${displayName === "" || userTag === "" || detail === ""? "text-gray-400 dark:text-gray-600 hover:bg-transparent dark:hover:bg-transparent" : ""}`}>保存</button>
-                </div>
+                    <div className="mt-3 flex justify-end">
+                        <button type="submit" disabled={displayName === "" || userTag === "" || detail === ""} className={`font-bold p-3 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 ${displayName === "" || userTag === "" || detail === ""? "text-gray-400 dark:text-gray-600 hover:bg-transparent dark:hover:bg-transparent" : ""}`}>保存</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
