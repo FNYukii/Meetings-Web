1cd5481742c6f2ef74b10300d7289749319e3ace
Yu357 <forestgreen14689@gmail.com>
2022-10-18 23:46:11 +0900

    feat: ロード中はProgressImageを表示

--- src/components/modals/EditUserModal.tsx
+++ src/components/modals/EditUserModal.tsx
@@ -3,6 +3,7 @@ import { MdOutlineClose } from "react-icons/md"
 import { useNavigate } from "react-router-dom"
 import FireAuth from "../../utilities/FireAuth"
 import FireUsers from "../../utilities/FireUsers"
+import ProgressImage from "../parts/images/ProgressImage"
 
 export default function EditUserModal() {
 
@@ -10,10 +11,29 @@ export default function EditUserModal() {
     const [displayName, setDisplayName] = useState("")
     const [userTag, setUserTag] = useState("")
     const [introduction, setIntroduction] = useState("")
+    const body = document.body
+
+    const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
         readUser()
+
+        document.title = "プロフィールを編集 - Meetings"
+        document.addEventListener("keydown", onKeyDown, false)
+        body.style.overflowY = "hidden"
+        
+        return () => {
+            body.style.overflowY = ""
+            document.removeEventListener("keydown", onKeyDown, false)
+        }
     })
+
+    const onKeyDown = (event: KeyboardEvent) => {
+
+        if (event.key === "Escape") {
+            navigate(-1)
+        }
+    }
 
     async function readUser() {
 
@@ -30,6 +50,7 @@ export default function EditUserModal() {
         setDisplayName(user.displayName)
         setUserTag(user.userTag)
         setIntroduction(user.introduction)
+        setIsLoaded(true)
     }
 
     function onSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -49,19 +70,27 @@ export default function EditUserModal() {
                     <MdOutlineClose className="text-2xl text-gray-500" />
                 </button>
 
-                <form onSubmit={(e) => onSubmit(e)}>
-                    <div className="px-3 mt-3">
-
-                        <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} placeholder="ディスプレイネーム" className="p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
-                        <input type="text" value={userTag} onChange={(e) => setUserTag(e.target.value)} placeholder="ユーザータグ" className="mt-5 p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
-
-                        <textarea value={introduction} onChange={(e) => setIntroduction(e.target.value)} placeholder="自己紹介" className="mt-5 h-24 resize-none p-3 border rounded-md border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500 w-full" />
+                {!isLoaded &&
+                    <div className='flex justify-center p-3'>
+                        <ProgressImage />
                     </div>
+                }
 
-                    <div className="mt-3 flex justify-end">
-                        <button type="submit" disabled={displayName === "" || userTag === "" || introduction === ""} className={`font-bold p-3 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 ${displayName === "" || userTag === "" || introduction === ""? "text-gray-400 dark:text-gray-600 hover:bg-transparent dark:hover:bg-transparent" : ""}`}>保存</button>
-                    </div>
-                </form>
+                {isLoaded &&
+                    <form onSubmit={(e) => onSubmit(e)}>
+                        <div className="px-3 mt-3">
+
+                            <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} placeholder="ディスプレイネーム" className="p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
+                            <input type="text" value={userTag} onChange={(e) => setUserTag(e.target.value)} placeholder="ユーザータグ" className="mt-5 p-2 w-full rounded-md border border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500" />
+
+                            <textarea value={introduction} onChange={(e) => setIntroduction(e.target.value)} placeholder="自己紹介" className="mt-5 h-24 resize-none p-3 border rounded-md border-gray-400 dark:border-gray-600 bg-transparent placeholder:text-gray-500 w-full" />
+                        </div>
+
+                        <div className="mt-3 flex justify-end">
+                            <button type="submit" disabled={displayName === "" || userTag === "" || introduction === ""} className={`font-bold p-3 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 ${displayName === "" || userTag === "" || introduction === "" ? "text-gray-400 dark:text-gray-600 hover:bg-transparent dark:hover:bg-transparent" : ""}`}>保存</button>
+                        </div>
+                    </form>
+                }
             </div>
         </div>
     )
