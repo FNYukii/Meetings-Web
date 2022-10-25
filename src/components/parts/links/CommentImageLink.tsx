e4157856fbcdc374532357d3f65772c0fb808045
Yu357 <forestgreen14689@gmail.com>
2022-10-25 15:04:38 +0900

    feat: CommentImageLinkにtransitionを設定

--- src/components/parts/links/CommentImageLink.tsx
+++ src/components/parts/links/CommentImageLink.tsx
@@ -9,7 +9,7 @@ export default function CommentImageLink(props: { comment: Comment, imageIndex: 
         <Link to={`/comments/${props.comment.id}/images/${props.imageIndex + 1}`} state={{ previousPath: location.pathname }} className="pointer-events-auto relative">
 
             <img src={props.comment.imageUrls[props.imageIndex]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
-            <div className="absolute top-0 left-0 hover:bg-black/10 dark:hover:bg-white/10 w-full h-full rounded-xl"></div>
+            <div className="absolute top-0 left-0 hover:bg-black/10 dark:hover:bg-white/10 transition w-full h-full rounded-xl"></div>
         </Link>
     )
 }
