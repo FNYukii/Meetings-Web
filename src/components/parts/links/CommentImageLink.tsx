4c31ed59f1da282adb7ff978805b45e8ae256cb7
Yu357 <forestgreen14689@gmail.com>
2022-10-25 15:16:30 +0900

    feat: Reactコンポーネントの新しい書き方を試した

--- src/components/parts/links/CommentImageLink.tsx
+++ src/components/parts/links/CommentImageLink.tsx
@@ -1,15 +1,23 @@
+import { FC } from "react"
 import { Link, useLocation } from "react-router-dom";
 import Comment from "../../../entities/Comment";
 
-export default function CommentImageLink(props: { comment: Comment, imageIndex: number }) {
+interface Props {
+    comment: Comment;
+    imageIndex: number
+}
 
+const CommentImageLink: FC<Props> = ({comment, imageIndex}) => {
+    
     const location = useLocation()
 
     return (
-        <Link to={`/comments/${props.comment.id}/images/${props.imageIndex + 1}`} state={{ previousPath: location.pathname }} className="pointer-events-auto relative">
+        <Link to={`/comments/${comment.id}/images/${imageIndex + 1}`} state={{ previousPath: location.pathname }} className="pointer-events-auto relative">
 
-            <img src={props.comment.imageUrls[props.imageIndex]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
+            <img src={comment.imageUrls[imageIndex]} alt="Attached to comment" className="rounded-xl w-full h-full object-cover border border-zinc-200 dark:border-zinc-800" />
             <div className="absolute top-0 left-0 hover:bg-black/10 dark:hover:bg-white/10 transition w-full h-full rounded-xl"></div>
         </Link>
     )
 }
+
+export default CommentImageLink;
