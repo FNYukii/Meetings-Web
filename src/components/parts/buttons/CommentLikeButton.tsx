0cebf27f4f67e1ee76da16895277a2e12b2118d9
Yu357 <forestgreen14689@gmail.com>
2022-10-25 17:16:58 +0900

    feat: 自分がいいねしているときはボタンが赤色になるようにした

--- src/components/parts/buttons/CommentLikeButton.tsx
+++ src/components/parts/buttons/CommentLikeButton.tsx
@@ -1,21 +1,30 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { AiOutlineExclamationCircle, AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineExclamationCircle, AiOutlineHeart } from "react-icons/ai";
 import Comment from "../../../entities/Comment";
 import User from "../../../entities/User";
+import FireAuth from "../../../utilities/FireAuth";
 import { auth } from "../../../utilities/firebase";
 import FireUsers from "../../../utilities/FireUsers";
 
-export default function CommentLikeButton(props: { comment: Comment, isReadFromSeaver: boolean, className?: string}) {
+export default function CommentLikeButton(props: { comment: Comment, isReadFromSeaver: boolean, className?: string }) {
 
     const [likedUsers, setLikedUsers] = useState<User[] | null>(null)
     const [isLoaded, setIsLoaded] = useState(false)
 
     const [uid, setUid] = useState<string | null>(null)
 
+    const [likedCommentIds, setLikedCommentIds] = useState<string[] | null>(null)
+
     useEffect(() => {
         readLikedUsers()
+        readLikedCommentIds()
+        startReadingAuthState()
 
+        // eslint-disable-next-line
+    }, [])
+
+    function startReadingAuthState() {
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 setUid(user.uid)
@@ -23,12 +32,10 @@ export default function CommentLikeButton(props: { comment: Comment, isReadFromS
                 setUid(null)
             }
         })
-
-        // eslint-disable-next-line
-    }, [])
+    }
 
     async function readLikedUsers() {
-        
+
         let likedUsers = await FireUsers.readLikedUsersFromCache(props.comment.id)
         setLikedUsers(likedUsers)
         setIsLoaded(true)
@@ -38,6 +45,24 @@ export default function CommentLikeButton(props: { comment: Comment, isReadFromS
             setLikedUsers(likedUsers)
             setIsLoaded(true)
         }
+    }
+
+    async function readLikedCommentIds() {
+
+        // UIDを読み取り
+        const uid = FireAuth.uid()
+        if (uid === null) {
+            return
+        }
+
+        // Userを読み取り
+        const user = await FireUsers.readUser(uid)
+        if (user === null) {
+            return
+        }
+
+        const likedCommentIds = user.likedCommentIds
+        setLikedCommentIds(likedCommentIds)
     }
 
     async function onClick() {
@@ -66,13 +91,14 @@ export default function CommentLikeButton(props: { comment: Comment, isReadFromS
         if (likedCommentIds.includes(props.comment.id)) {
 
             FireUsers.unlikeComment(props.comment.id)
-            
+
         } else {
 
             FireUsers.likeComment(props.comment.id)
         }
 
         readLikedUsers()
+        readLikedCommentIds()
     }
 
     return (
@@ -89,16 +115,28 @@ export default function CommentLikeButton(props: { comment: Comment, isReadFromS
 
             {isLoaded && likedUsers === null &&
                 <div>
-                    <AiOutlineExclamationCircle className="text-gray-500 pointer-events-auto text-xl"/>
+                    <AiOutlineExclamationCircle className="text-gray-500 pointer-events-auto text-xl" />
                 </div>
             }
 
             {isLoaded && likedUsers !== null &&
                 <div className="flex">
 
-                    <button onClick={() => onClick()} disabled={uid === null} className={`z-10 flex items-center p-1 rounded-full pointer-events-auto ${uid === null ? "" : "hover:bg-zinc-100 dark:hover:bg-zinc-900 transition"}`}>
-                        <AiOutlineHeart className="text-xl text-gray-500 " />
-                        <span className="text-gray-500 ml-1">{likedUsers?.length ?? "0"}</span>
+                    <button onClick={() => onClick()} disabled={uid === null} className={`z-10 p-1 rounded-full pointer-events-auto ${uid === null ? "" : "hover:bg-zinc-100 dark:hover:bg-zinc-900 transition"}`}>
+
+                        {!likedCommentIds?.includes(props.comment.id) &&
+                            <div className="flex items-center">
+                                <AiOutlineHeart className="text-xl text-gray-500" />
+                                <span className="text-gray-500 ml-1">{likedUsers?.length ?? "0"}</span>
+                            </div>
+                        }
+
+                        {likedCommentIds?.includes(props.comment.id) &&
+                            <div className="flex items-center">
+                                <AiFillHeart className="text-xl text-red-500" />
+                                <span className="text-red-500 ml-1">{likedUsers?.length ?? "0"}</span>
+                            </div>
+                        }
                     </button>
                 </div>
             }
