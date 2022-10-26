51155f897b3735284bef462fa6205481d45d15ee
Yu357 <forestgreen14689@gmail.com>
2022-10-27 00:28:22 +0900

    feat: スタイリング

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 export default function App() {
     
     return (
-        <div className='flex mx-auto xl:width-1280 w-full'>
+        <div className='flex mx-auto xl:width-1200 w-full'>
             <BrowserRouter>
                 <LeftColumn className='xl:w-1/4 md:w-1/12'/>
                 <CenterColumn className='xl:w-2/4 md:w-7/12 w-full'/>
