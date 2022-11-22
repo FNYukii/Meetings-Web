4db30bb1e4a8a627480253bfee3d4c00fa950539
Yu357 <forestgreen14689@gmail.com>
2022-11-22 16:42:02 +0900

    feat: ランダムなファイル名を生成するようにした

--- src/utilities/FireImages.ts
+++ src/utilities/FireImages.ts
@@ -1,12 +1,18 @@
 import { ref, uploadBytes } from "firebase/storage"
 import { storage } from "./firebase"
+import { v4 } from "uuid"
 
 class FireImages {
 
     static async uploadIconImage(file: File): Promise<string | null> {
 
-        const storageRef = ref(storage, `icons/${file.name}`)
+        // UUIDを使ってランダムなファイル名を生成
+        const fileName: string = `${v4}.jpg`
 
+        // Firebase Cloud Storageの参照を作成
+        const storageRef = ref(storage, `icons/${fileName}`)
+
+        // ファイルをアップロード
         return await uploadBytes(storageRef, file)
             .then((snapshot) => {
                 console.log("Uploaded 1 file.");
