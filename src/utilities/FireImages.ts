41c1e635f5915f83afbebd5a5a245aca3cffcf3b
Yu357 <forestgreen14689@gmail.com>
2022-11-22 14:55:29 +0900

    feat: 参照を作成してみた

--- src/utilities/FireImages.ts
+++ src/utilities/FireImages.ts
@@ -1,7 +1,10 @@
+import { ref } from "firebase/storage"
+import { storage } from "./firebase"
+
 class FireImages {
 
     static uploadImage(image: string) {
-
+        const storageRef = ref(storage, `icons/sample`)
     }
 }
 
