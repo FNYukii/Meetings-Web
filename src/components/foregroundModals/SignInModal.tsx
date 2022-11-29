824c85df742e4e4d0eb0836637fa98dc11e0f8bd
Yu357 <forestgreen14689@gmail.com>
2022-11-29 13:59:19 +0900

    clean: ReportModalとSignInModalを改良

--- src/components/foregroundModals/SignInModal.tsx
+++ src/components/foregroundModals/SignInModal.tsx
@@ -1,46 +1,15 @@
-import { useNavigate } from "react-router-dom"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import SignInSection from "../parts/sections/SignInSection"
 import SignUpSection from "../parts/sections/SignUpSection"
-import CloseButton from "../parts/buttons/CloseButton"
+import FormModal from "../parts/modals/FormModal"
 
 function SignInModal() {
 
-    const navigate = useNavigate()
-    const body = document.body
-
     const [isShowSignUpSection, setIsShowSignUpSection] = useState(false)
 
-    useEffect(() => {
-
-        document.title = "サインイン - Meetings"
-        document.addEventListener("keydown", onKeyDown, false)
-        body.style.overflowY = "hidden"
-
-        return () => {
-            body.style.overflowY = ""
-            document.removeEventListener("keydown", onKeyDown, false)
-        }
-
-        // eslint-disable-next-line
-    }, [])
-
-    const onKeyDown = (event: KeyboardEvent) => {
-
-        if (event.key === "Escape") {
-            navigate(-1)
-        }
-    }
-
     return (
-        <div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
-
-            <div onClick={() => navigate(-1)} className="w-full h-full bg-black/20 dark:bg-white/20"></div>
-
-            <div className="absolute bg-white dark:bg-black p-6 rounded-xl md:width-600 w-11/12 max-height-screen-90">
-
-                <CloseButton />
-
+        <FormModal title="サインイン - Meetings">
+            <div>
                 {!isShowSignUpSection &&
                     <SignInSection setIsShowSignUpSection={setIsShowSignUpSection} />
                 }
@@ -49,7 +18,7 @@ function SignInModal() {
                     <SignUpSection setIsShowSignUpSection={setIsShowSignUpSection} />
                 }
             </div>
-        </div>
+        </FormModal>
     )
 }
 
