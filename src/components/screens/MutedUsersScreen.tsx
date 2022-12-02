d032f5e2594c67360b7d40f5131c3b42e5c48ef3
Yu357 <forestgreen14689@gmail.com>
2022-12-02 22:12:40 +0900

    feat: マージン調整

--- src/components/screens/MutedUsersScreen.tsx
+++ src/components/screens/MutedUsersScreen.tsx
@@ -12,7 +12,7 @@ function MutedUsersScreen() {
 
                     <div className="flex items-center">
                         <BackButton />
-                        <span className='font-bold text-lg ml-11'>ミュート中のユーザー</span>
+                        <span className='font-bold text-lg ml-7'>ミュート中のユーザー</span>
                     </div>
                 </div>
             </div>
