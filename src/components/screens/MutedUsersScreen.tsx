c37eeca275d8aed766a52455e7bcadffe005df3b
Yu357 <forestgreen14689@gmail.com>
2022-12-02 21:30:48 +0900

    feat: タイトルバーを配置

--- src/components/screens/MutedUsersScreen.tsx
+++ src/components/screens/MutedUsersScreen.tsx
@@ -1,8 +1,20 @@
+import BackButton from "../parts/buttons/BackButton"
+
 function MutedUsersScreen() {
 
     return (
         <div>
-            <h1>MutedUsersScreen</h1>
+
+            <div className='sticky top-0 z-20'>
+                <div className='relative h-14 pl-1 pr-3 flex items-center justify-between bg-white/70 dark:bg-black/70 backdrop-blur'>
+                    <div className='absolute top-0 left-0 w-full h-full cursor-pointer' onClick={() => window.scrollTo(0, 0)}></div>
+
+                    <div className="flex items-center">
+                        <BackButton />
+                        <span className='font-bold text-lg ml-11'>ミュート中のユーザー</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
