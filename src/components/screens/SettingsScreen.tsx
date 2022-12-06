601a5a0e1a169fa5f25073a6c3f9b59b01061518
Yu357 <forestgreen14689@gmail.com>
2022-12-06 15:29:19 +0900

    feat: タイトルバーを配置

--- src/components/screens/SettingsScreen.tsx
+++ src/components/screens/SettingsScreen.tsx
@@ -2,7 +2,16 @@ function SettingsScreen() {
 
     return (
         <div>
-            <h1>Settings</h1>
+            <div className='sticky top-0 z-20'>
+                <div className='relative h-14 pl-1 pr-3 flex items-center justify-between bg-white/70 dark:bg-black/70 backdrop-blur'>
+                    <div className='absolute top-0 left-0 w-full h-full cursor-pointer' onClick={() => window.scrollTo(0, 0)}></div>
+
+                    <div className="flex items-center">
+                        <span className='font-bold text-lg ml-2'>設定</span>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
