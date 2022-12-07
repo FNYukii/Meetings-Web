2cd144294228f1fc18ab8d2f6fe2cc86546329bd
Yu357 <forestgreen14689@gmail.com>
2022-12-07 14:42:16 +0900

    clean: NotFoundScreenをリファクタリング

--- src/components/screens/NotFoundScreen.tsx
+++ src/components/screens/NotFoundScreen.tsx
@@ -1,4 +1,5 @@
 import BackButton from "../parts/buttons/BackButton"
+import TitleBar from "../parts/sections/TitleBar"
 
 function NotFoundScreen() {
 
@@ -6,16 +7,10 @@ function NotFoundScreen() {
 
     return (
         <div>
-            <div className='sticky top-0 z-20'>
-                <div className='relative h-14 px-3 flex items-center bg-white/70 dark:bg-black/70 backdrop-blur'>
-
-                    <div className='absolute top-0 left-0 w-full h-full cursor-pointer' onClick={() => window.scrollTo(0, 0)}></div>
-
-                    <BackButton/>
-                    
-                    <span className='font-bold text-lg'>Not Found</span>
-                </div>
-            </div>
+            <TitleBar>
+                <BackButton className="ml-1"/>
+                <span className='ml-1 font-bold text-lg'>Not Found</span>
+            </TitleBar>
 
             <div className="p-2">
                 <p className="text-gray-500 text-center">ページが見つかりませんでした。</p>
