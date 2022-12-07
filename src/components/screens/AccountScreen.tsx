0a4c8d89a73677de6b72c597e433252170f2819b
Yu357 <forestgreen14689@gmail.com>
2022-12-07 21:37:01 +0900

    feat: メールアドレス表示用のUI部品を配置

--- src/components/screens/AccountScreen.tsx
+++ src/components/screens/AccountScreen.tsx
@@ -1,3 +1,6 @@
+import { AiOutlineMail } from "react-icons/ai"
+import SignOutButton from "../parts/buttons/SignOutButton"
+import SettingsItemLink from "../parts/links/SettingsItemLink"
 import TitleBar from "../parts/sections/TitleBar"
 
 function AccountScreen() {
@@ -7,6 +10,12 @@ function AccountScreen() {
             <TitleBar showBackButton>
                 <span className='ml-1 font-bold text-lg'>アカウント</span>
             </TitleBar>
+
+            <SettingsItemLink title="メールアドレス" subTitle="xxx.xxxxx.xx@gmail.com" to="/settings/account/email">
+                <AiOutlineMail className="text-gray-500 text-2xl" />
+            </SettingsItemLink>
+
+            <SignOutButton />
         </div>
     )
 }
