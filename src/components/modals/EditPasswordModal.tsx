68a5e70bb1e5d4aca756ea5dba9dfd61e843cb42
Yu357 <forestgreen14689@gmail.com>
2022-12-15 11:11:36 +0900

    feat: EditPasswordModal.tsxを作成

---
+++ src/components/modals/EditPasswordModal.tsx
@@ -0,0 +1,12 @@
+import FormModal from "../parts/modals/FormModal"
+
+function EditPasswordModal() {
+
+    return (
+        <FormModal title="パスワードを編集 - Meetings">
+            <span>パスワードを編集</span>
+        </FormModal>
+    )
+}
+
+export default EditPasswordModal
