afcc447f6b4c8d92d7b09f35c6967bbdbab324b2
Yu357 <forestgreen14689@gmail.com>
2022-12-15 11:32:20 +0900

    feat: 完了ボタンを配置

--- src/components/modals/EditPasswordModal.tsx
+++ src/components/modals/EditPasswordModal.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import SubmitButton from "../parts/buttons/SubmitButton"
 import FormModal from "../parts/modals/FormModal"
 
 function EditPasswordModal() {
@@ -13,14 +14,20 @@ function EditPasswordModal() {
     return (
         <FormModal title="パスワードを変更 - Meetings">
 
-            <div className="px-3">
-                <p className="text-2xl font-bold">パスワードを変更</p>
+            <form>
+                <div className="px-3">
+                    <p className="text-2xl font-bold">パスワードを変更</p>
 
-                <input type="password" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} placeholder="現在のパスワード" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
-                
-                <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="新しいパスワード" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
-                <input type="password" value={newPassworConfirm} onChange={(e) => setNewPasswordConfirm(e.target.value)} placeholder="新しいパスワードを確認" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
-            </div>
+                    <input type="password" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} placeholder="現在のパスワード" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
+
+                    <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="新しいパスワード" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
+                    <input type="password" value={newPassworConfirm} onChange={(e) => setNewPasswordConfirm(e.target.value)} placeholder="新しいパスワードを確認" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
+                </div>
+
+                <div className="mt-3 pl-3 flex justify-end items-center">
+                    <SubmitButton text="完了" disabled={currentPassword === "" || newPassword === "" || newPassword !== newPassworConfirm} isLoading={isLoading} />
+                </div>
+            </form>
         </FormModal>
     )
 }
