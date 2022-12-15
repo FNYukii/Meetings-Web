2e6f858ad7119a579854bc3156ef892a02aec0b5
Yu357 <forestgreen14689@gmail.com>
2022-12-15 10:36:48 +0900

    feat: inputタグを配置

--- src/components/modals/EditEmailModal.tsx
+++ src/components/modals/EditEmailModal.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import FormModal from "../parts/modals/FormModal"
 
 function EditEmailModal() {
+
+    const [currentEmail, setCurrentEmailmail] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+
+    const [password, setPassword] = useState("")
 
     return (
         <FormModal title="メールアドレスを編集 - Meetings">
             <div className="px-3">
                 <p className="text-2xl font-bold">メールアドレスを編集</p>
+
+                <input type="email" value={currentEmail} onChange={(e) => setCurrentEmailmail(e.target.value)} placeholder="現在のメールアドレス" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
+                <input type="email" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} placeholder="新しいメールアドレス" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
+
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="パスワード" className="mt-5 w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600" />
             </div>
         </FormModal>
     )
