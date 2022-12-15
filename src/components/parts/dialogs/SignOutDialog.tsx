4b39b61a8986ed2c26241b487ab09d5e6f093ffc
Yu357 <forestgreen14689@gmail.com>
2022-12-15 12:07:55 +0900

    feat: ダイアログの影を押すことで閉じることができるようにした

--- src/components/parts/dialogs/SignOutDialog.tsx
+++ src/components/parts/dialogs/SignOutDialog.tsx
@@ -5,7 +5,7 @@ function SignOutDialog(props: {setIsShowDialog: React.Dispatch<React.SetStateAct
     return (
         <div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
 
-            <div className="w-full h-full bg-black/20 dark:bg-white/20"></div>
+            <div onClick={() => props.setIsShowDialog(false)} className="w-full h-full bg-black/20 dark:bg-white/20"></div>
 
             <div className="absolute bg-white dark:bg-black p-6 rounded-xl md:width-600 w-11/12 max-height-screen-90 overflow-y-scroll">
 
