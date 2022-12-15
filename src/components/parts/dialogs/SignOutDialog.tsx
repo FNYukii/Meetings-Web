f02ce687e66dd91f4c17afa6f4c919425a5b622d
Yu357 <forestgreen14689@gmail.com>
2022-12-15 12:21:15 +0900

    feat: スタイリング

--- src/components/parts/dialogs/SignOutDialog.tsx
+++ src/components/parts/dialogs/SignOutDialog.tsx
@@ -1,6 +1,6 @@
 import { MdOutlineClose } from "react-icons/md"
 
-function SignOutDialog(props: {setIsShowDialog: React.Dispatch<React.SetStateAction<boolean>>}) {
+function SignOutDialog(props: { setIsShowDialog: React.Dispatch<React.SetStateAction<boolean>> }) {
 
     return (
         <div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
@@ -14,7 +14,13 @@ function SignOutDialog(props: {setIsShowDialog: React.Dispatch<React.SetStateAct
                 </button>
 
                 <div className="px-3">
-                    <p>サインアウトしてもよろしいですか?</p>
+                    <p className="text-xl font-bold">サインアウトしてもよろしいですか?</p>
+                </div>
+
+                <div className="flex justify-between">
+
+                    <button onClick={() => props.setIsShowDialog(false)} className="py-1 px-3 rounded-full hover:bg-gray-100 dark:hover:bg-gray-900">キャンセル</button>
+                    <button className="text-red-500 py-1 px-3 font-bold rounded-full hover:bg-red-100 dark:hover:bg-red-900">サインアウト</button>
                 </div>
             </div>
         </div>
