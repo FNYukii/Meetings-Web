795ad21c02b54f3dd8e4b31d7e55f5c4456884ff
Yu357 <forestgreen14689@gmail.com>
2022-12-15 10:21:54 +0900

    feat: EditEmailModalを作成

---
+++ src/components/modals/EditEmailModal.tsx
@@ -0,0 +1,14 @@
+import FormModal from "../parts/modals/FormModal"
+
+function EditEmailModal() {
+
+    return (
+        <FormModal title="メールアドレスを編集 - Meetings">
+            <div className="px-3">
+                <p className="text-2xl font-bold">メールアドレスを編集</p>
+            </div>
+        </FormModal>
+    )
+}
+
+export default EditEmailModal
