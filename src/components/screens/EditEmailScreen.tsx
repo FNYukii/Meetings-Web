4ddf1fa06d95cbfab626774e7df3f34d665a5cbd
Yu357 <forestgreen14689@gmail.com>
2022-12-15 10:03:20 +0900

    feat: EditEmailScreenを作成

---
+++ src/components/screens/EditEmailScreen.tsx
@@ -0,0 +1,12 @@
+import TitleBar from "../parts/sections/TitleBar";
+
+function EditEmailScreen() {
+
+    return (
+        <div>
+            <TitleBar showBackButton>
+                <span>メールアドレスを編集</span>
+            </TitleBar>
+        </div>
+    )
+}
