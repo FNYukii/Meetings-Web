aea5a376307b1fd33cec30d058037f692835898b
Yu357 <forestgreen14689@gmail.com>
2022-12-18 00:18:50 +0900

    feat: パスワードの可視性を切り替えられるようにした

--- src/components/parts/inputs/PasswordInput.tsx
+++ src/components/parts/inputs/PasswordInput.tsx
@@ -9,20 +9,20 @@ function PasswordInput(props: { value: string, onChange: React.Dispatch<React.Se
         <div className={`flex items-center ${props.className}`}>
 
             <input
-                type="password"
+                type={isPasswordVisible ? "text" : "password"}
                 value={props.value}
                 onChange={(e) => props.onChange(e.target.value)}
                 placeholder={props.placeholder ?? "パスワード"}
                 className="w-full py-2 bg-transparent border-b border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500 placeholder:text-gray-400 dark:placeholder:text-gray-600"
             />
 
-            <button type="button">
+            <button type="button" onClick={() => setIsPasswordVisible(!isPasswordVisible)} className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-900">
                 {!isPasswordVisible &&
-                    <AiOutlineEye />
+                    <AiOutlineEyeInvisible className="text-xl text-gray-500" />
                 }
 
                 {isPasswordVisible &&
-                    <AiOutlineEyeInvisible />
+                    <AiOutlineEye className="text-xl text-gray-500" />
                 }
             </button>
         </div>
